0d8b8fcb79cd6fe7164be2f07b8c70349163a1a4
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-04-11 00:11:50 +0900

    feat: impl use detect wallet

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from 'antd'
 import { useListAllocatorAllocationResultsQuery } from '@dev/graphql'
+import { useDetectWallet } from 'src/fixtures/wallet/hooks'
 import Router from 'next/router'
 
 type InitialProps = {}
@@ -9,9 +10,13 @@ type Props = {} & InitialProps
 const Index = (_: Props) => {
   const { data, loading } = useListAllocatorAllocationResultsQuery({})
 
+  const { isConnected } = useDetectWallet()
+
   return (
     <div>
       <Button onClick={() => Router.push('/about')}>please click here!</Button>
+      {isConnected && <div>wallet connected</div>}
+      {!isConnected && <div>no wallet connected</div>}
       {loading && <div>loading.......</div>}
       {data && (
         <div>
