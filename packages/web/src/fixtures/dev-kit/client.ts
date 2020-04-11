454265b2c29f8e91e3dc45b37b5172ab42b40a10
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-04-11 22:19:05 +0900

    chore: update devkit

--- packages/web/src/fixtures/dev-kit/client.ts
+++ packages/web/src/fixtures/dev-kit/client.ts
@@ -1,7 +1,6 @@
 import { contractFactory } from '@dev-protocol/dev-kit-js'
 
 export const NewClient = () => {
-  console.log(`teaetateatea`)
   const { ethereum } = window
   if (ethereum) {
     return contractFactory(ethereum)
