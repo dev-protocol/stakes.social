c88a71bf75c8d11449596c1b2ed678992f958966
k3nt0w <kento.watanabe.11@gmail.com>
2020-04-12 02:19:32 +0900

    chore: jest config

---
+++ packages/web/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  clearMocks: true,
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^src/(.+)': '<rootDir>/src/$1',
+    '\\.(css|less)$': 'identity-obj-proxy'
+  }
+}
