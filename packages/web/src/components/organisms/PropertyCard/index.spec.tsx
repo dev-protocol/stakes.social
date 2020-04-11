7ce4c0ed60338677042c5ed4545405a2c17a615a
k3nt0w <kento.watanabe.11@gmail.com>
2020-04-12 03:05:44 +0900

    refactor: file name

--- packages/web/src/components/organisms/PropertyCard/index.spec.tsx
+++ packages/web/src/components/organisms/PropertyCard/index.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { PropertyCard } from './PropertyCard'
+import { PropertyCard } from './'
 
 jest.mock('src/fixtures/dev-kit/hooks')
 
--- packages/web/src/components/organisms/PropertyCard/index.ts
+++
@@ -1,1 +0,0 @@
-export { PropertyCard } from './PropertyCard'
