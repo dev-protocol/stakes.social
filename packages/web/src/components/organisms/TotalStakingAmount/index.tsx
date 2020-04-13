027eac8570e5229ee071a6efedee6ddac8c6ba77
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-04-14 04:06:55 +0900

    feat: added total staking org

---
+++ packages/web/src/components/organisms/TotalStakingAmount/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Card } from 'antd'
+import { useGetTotalStakingAmount } from 'src/fixtures/dev-kit/hooks'
+
+interface Props {
+  propertyAddress: string
+}
+
+export const TotalStakingAmount = ({ propertyAddress }: Props) => {
+  const { totalStakingAmount } = useGetTotalStakingAmount(propertyAddress)
+
+  return <Card>{totalStakingAmount && <div>total staking amount: {totalStakingAmount.dp(1).toNumber()}</div>}</Card>
+}
--- packages/web/src/pages/[propertyAddress].tsx
+++ packages/web/src/pages/[propertyAddress].tsx
@@ -2,27 +2,24 @@ import React from 'react'
 import { Button } from 'antd'
 import Router, { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
-import { useGetTotalStakingAmount } from 'src/fixtures/dev-kit/hooks'
+import { TotalStakingAmount } from 'src/components/organisms/TotalStakingAmount'
 
 const WalletConnectButton = dynamic(
   () => import('src/components/organisms/WalletConnectButton').then(mod => mod.WalletConnectButton) as any,
   { ssr: false }
 )
 
-type InitialProps = {}
-
-type Props = {} & InitialProps
+type Props = {}
 
 const PropertyAddressDetail = (_: Props) => {
   const { propertyAddress } = useRouter().query as { propertyAddress: string }
-
-  const { totalStakingAmount } = useGetTotalStakingAmount(propertyAddress)
 
   return (
     <div>
       <Button onClick={() => Router.push('/')}>please click here!</Button>
       <WalletConnectButton />
-      {totalStakingAmount && <div>total staking amount: {totalStakingAmount.dp(1).toNumber()}</div>}
+      <p>property address: {propertyAddress}</p>
+      <TotalStakingAmount propertyAddress={propertyAddress} />
     </div>
   )
 }
