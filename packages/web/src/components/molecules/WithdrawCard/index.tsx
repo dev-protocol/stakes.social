a06d9c777be23013de6d85fbfd85a22faa530cca
k3nt0w <kento.watanabe.11@gmail.com>
2020-04-17 04:17:44 +0900

    wip: feat: space example

--- packages/web/src/components/molecules/WithdrawCard/index.tsx
+++ packages/web/src/components/molecules/WithdrawCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import BigNumber from 'bignumber.js'
-import { Card, Button } from 'antd'
+import { Card, Button, Space } from 'antd'
 
 interface Props {
   label: 'Staking' | 'Holder'
@@ -11,19 +11,22 @@ interface Props {
 
 export const WithdrawCard = ({ amount, label, onSubmitWithdraw, lastUpdate }: Props) => {
   return (
-    <Card
-      title={`Withdraw ${label} Reward`}
-      extra={
-        <>
+    <Card style={{ maxWidth: 961 }}>
+      <Space direction="horizontal" size={200}>
+        <Space direction="vertical" size={32}>
+          <div style={{ fontSize: '24px', lineHeight: '32px', color: '#000' }}>Withdraw {label} Reward</div>
+          <div style={{ fontSize: '24px', lineHeight: '32px', color: 'rgba(0, 0, 0, 0.85)' }}>
+            {amount && amount.dp(1).toNumber()} DEV
+          </div>
+        </Space>
+        <Space direction="vertical" size={32}>
           {lastUpdate && <p>Last Update: {lastUpdate}</p>}
           <a href="#" target="_blank" rel="noreferrer noopener">
             Mining now
           </a>
-        </>
-      }
-    >
-      <p>{amount && amount.dp(1).toNumber()} DEV</p>
-      <Button onClick={onSubmitWithdraw}>Withdraw</Button>
+          <Button onClick={onSubmitWithdraw}>Withdraw</Button>
+        </Space>
+      </Space>
     </Card>
   )
 }
