636999ba9e4b50e1e79f09eb36bf4aa374f2fed6
Kento Watanabe <kento.watanabe.11@gmail.com>
2020-04-20 05:11:57 +0900

    feat: main header (#64)
    * feat: main header
    
    * refactor: refine ui
    
    * test: update snapshot

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { PropertyCardList } from 'src/components/organisms/PropertyCardList'
+import { MainHeader } from 'src/components/organisms/MainHeader'
 import { Footer } from 'src/components/organisms/Footer'
 
 type InitialProps = {}
@@ -8,6 +9,7 @@ type Props = {} & InitialProps
 const Index = (_: Props) => {
   return (
     <>
+      <MainHeader />
       <div style={{ maxWidth: '1048px', marginRight: 'auto', marginLeft: 'auto' }}>
         <PropertyCardList />
       </div>
