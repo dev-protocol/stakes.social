d3e67c2325f33bcaef038bc9ef6d4c794b3b0112
k3nt0w <kento.watanabe.11@gmail.com>
2020-04-25 21:16:01 +0900

    fix: formula

--- packages/web/src/fixtures/utility/gql-hooks-wrapper.ts
+++ packages/web/src/fixtures/utility/gql-hooks-wrapper.ts
@@ -4,7 +4,7 @@ export const useAverageInterestRate = (metrics: string) => {
   const { data: rewardCalculationResultData } = useGetRewardCalculationResultAggregateQuery({
     variables: { metricsList: [metrics] }
   })
-  const lockup = rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.lockup
   const stakingReward = rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.staking_reward
-  return lockup && stakingReward ? lockup / stakingReward : 0
+  const lockup = rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.lockup
+  return stakingReward && lockup ? stakingReward / lockup : 0
 }
