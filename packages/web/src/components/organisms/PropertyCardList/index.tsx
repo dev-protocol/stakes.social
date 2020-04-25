ffbcaf0a2a7294c6cfdf49947aa202683da75eff
k3nt0w <kento.watanabe.11@gmail.com>
2020-04-25 23:39:12 +0900

    feat: add loading spinner

--- packages/web/src/components/organisms/PropertyCardList/index.tsx
+++ packages/web/src/components/organisms/PropertyCardList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Spin } from 'antd'
 import { useListPropertyQuery } from '@dev/graphql'
 import { PropertyCard } from './PropertyCard'
 
@@ -7,7 +8,7 @@ export const PropertyCardList = () => {
 
   return (
     <div>
-      {loading && <div>loading.......</div>}
+      {loading && <Spin size="large" style={{ display: 'block', width: 'auto', padding: '100px' }} />}
       {data &&
         data.property_factory_create.map(d => (
           <div key={d.event_id} style={{ margin: '54px 0' }}>
