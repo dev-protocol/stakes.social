abe90f96ff74b5fcc025bbdbd24f5cf0ad06ad54
Kento Watanabe <kento.watanabe.11@gmail.com>
2020-04-25 22:40:28 +0900

    feat: error modal for checking Ethereum plugin (#90)
    * feat: error modal
    
    * chore: add comment

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Modal } from 'antd'
 import App, { AppInitialProps } from 'next/app'
 import { WithApolloProps } from 'next-with-apollo'
 import Head from 'next/head'
@@ -7,6 +8,17 @@ import withApollo from 'src/fixtures/withApollo'
 class NextApp extends App<AppInitialProps & WithApolloProps<{}>> {
   componentDidCatch = (error: Error, errorInfo: React.ErrorInfo) => {
     super.componentDidCatch(error, errorInfo)
+  }
+
+  componentDidMount = () => {
+    const { ethereum } = window
+    if (!ethereum) {
+      // TODO: fix design and messages.
+      Modal.error({
+        title: 'This is an error message',
+        content: 'some messages...some messages...'
+      })
+    }
   }
 
   render() {
