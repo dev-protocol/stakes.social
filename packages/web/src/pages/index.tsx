51ad846f7cafec4bb6b34d2a1a81a1e82f8559a6
Kento Watanabe <kento.watanabe.11@gmail.com>
2020-04-25 22:07:39 +0900

    feat: create new property button (#89)

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { PropertyCardList } from 'src/components/organisms/PropertyCardList'
 import { MainHeader } from 'src/components/organisms/MainHeader'
 import { Footer } from 'src/components/organisms/Footer'
+import { Button } from 'antd'
 
 type InitialProps = {}
 type Props = {} & InitialProps
@@ -11,6 +12,7 @@ const Index = (_: Props) => {
     <>
       <MainHeader />
       <div style={{ maxWidth: '1048px', marginRight: 'auto', marginLeft: 'auto' }}>
+        <Button style={{ display: 'block', margin: '32px 0 32px auto' }}>Create a new Property</Button>
         <PropertyCardList />
       </div>
       <Footer />
