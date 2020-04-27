03c38695b6de4fa742299968321d9d0f714cd836
aggre <hiroyuki.aggre@gmail.com>
2020-04-28 08:34:53 +0900

    implement styled Link component and BrandLogo component use that (#103)

---
+++ packages/web/src/components/atoms/A/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link, { LinkProps } from 'next/link'
+import styled from 'styled-components'
+
+const StyledLink = styled.a`
+  cursor: pointer;
+`
+
+export const A = (props: LinkProps) => (component: JSX.Element) => {
+  return (
+    <Link passHref {...props}>
+      <StyledLink>{component}</StyledLink>
+    </Link>
+  )
+}
