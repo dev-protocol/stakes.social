c7d1b1b7a61e4501fb43f538f73728e8542b66df
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-04-27 10:01:29 +0900

    fixed issue106 (#107)
    * feat: use bignumber for caluculation
    
    * chore: update snapshot
    
    * feat: added a hundred times

--- packages/web/src/fixtures/utility/gql-hooks-wrapper.ts
+++ packages/web/src/fixtures/utility/gql-hooks-wrapper.ts
@@ -1,10 +1,16 @@
 import { useGetRewardCalculationResultAggregateQuery } from '@dev/graphql'
+import BigNumber from 'bignumber.js'
 
 export const useAverageInterestRate = (metrics: string) => {
   const { data: rewardCalculationResultData } = useGetRewardCalculationResultAggregateQuery({
     variables: { metricsList: [metrics] }
   })
   const stakingReward = rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.staking_reward
+    ? new BigNumber(rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.staking_reward)
+    : undefined
   const lockup = rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.lockup
-  return stakingReward && lockup ? Math.round((1000 * stakingReward) / lockup) / 1000 : 0
+    ? new BigNumber(rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.lockup)
+    : undefined
+
+  return stakingReward && lockup ? lockup.div(stakingReward).times(100) : new BigNumber(0)
 }
