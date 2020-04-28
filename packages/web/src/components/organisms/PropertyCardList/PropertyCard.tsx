dba23428f6cd56cf1a0511a50407b1774fef8676
k3nt0w <kento.watanabe.11@gmail.com>
2020-04-29 02:35:43 +0900

    fix: decimal count

--- packages/web/src/components/organisms/PropertyCardList/PropertyCard.tsx
+++ packages/web/src/components/organisms/PropertyCardList/PropertyCard.tsx
@@ -70,7 +70,7 @@ export const PropertyCard = ({ propertyAddress }: Props) => {
               />
             </Col>
             <Col span={8}>
-              <Statistic title="Avg. Interest" value={averageInterestRate.dp(1).toNumber()} suffix="%" />
+              <Statistic title="Avg. Interest" value={averageInterestRate.dp(2).toNumber()} suffix="%" />
             </Col>
             <Col span={6}>
               {metrics && market ? <AssetStrength metrics={metrics} market={market} /> : <AssetStrengthWithoutData />}
