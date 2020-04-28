2aaf6f11729c8f14c2c28238d91a78a1e5f1d9bf
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-04-29 07:03:55 +0900

    feat: impl pagination

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { PropertyCardList } from 'src/components/organisms/PropertyCardList'
 import { MainHeader } from 'src/components/organisms/MainHeader'
 import { Footer } from 'src/components/organisms/Footer'
 import { Button } from 'antd'
+import { useRouter } from 'next/router'
 
 type InitialProps = {}
 type Props = {} & InitialProps
 
 const Index = (_: Props) => {
+  const router = useRouter()
+  const page = useMemo(() => {
+    const { page: pageStr } = router.query
+    if (typeof pageStr === 'string') {
+      return parseInt(pageStr)
+    }
+    return 1
+  }, [router])
+
   return (
     <>
       <MainHeader />
       <div style={{ maxWidth: '1048px', marginRight: 'auto', marginLeft: 'auto' }}>
         <Button style={{ display: 'block', margin: '32px 0 32px auto' }}>Create a new Property</Button>
-        <PropertyCardList />
+        <PropertyCardList currentPage={page} />
       </div>
       <Footer />
     </>
