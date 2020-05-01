b805f0fc7eaa243220aedffe51d725a33f9f8aa7
aggre <hiroyuki.aggre@gmail.com>
2020-05-01 12:46:18 +0900

    improve header's css

--- packages/web/src/components/organisms/Header/index.tsx
+++ packages/web/src/components/organisms/Header/index.tsx
@@ -12,7 +12,18 @@ const HeaderContainer = styled.div`
   display: grid;
   grid-auto-flow: column;
   justify-content: space-between;
-  padding: 3.8em;
+  align-items: center;
+  padding: 2em;
+  grid-gap: 1em;
+  svg {
+    width: 11em;
+  }
+  @media (min-width: 768px) {
+    padding: 3.8em;
+    svg {
+      width: 14em;
+    }
+  }
 `
 
 export const Header = ({ colorSchema = 'black' }: Props = {}) => {
@@ -23,8 +34,8 @@ export const Header = ({ colorSchema = 'black' }: Props = {}) => {
 
   return (
     <HeaderContainer>
-      <BrandLogo colorSchema={colorSchema} props={{ width: '14em', height: undefined }}></BrandLogo>
-      <Button size="large" disabled={isConnected} onClick={handleClick}>
+      <BrandLogo colorSchema={colorSchema} props={{ height: undefined }}></BrandLogo>
+      <Button size="middle" disabled={isConnected} onClick={handleClick}>
         {isConnected && 'Wallet connected'}
         {!isConnected && 'Connect to a wallet'}
       </Button>
