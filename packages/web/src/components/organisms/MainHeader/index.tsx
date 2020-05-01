3a05cb63bb37dce2613a7e3343ec86d68dfa3c58
aggre <hiroyuki.aggre@gmail.com>
2020-05-01 09:57:05 +0900

    add a link button to MainHeader

--- packages/web/src/components/organisms/MainHeader/index.tsx
+++ packages/web/src/components/organisms/MainHeader/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Header } from '../Header'
+import { Button } from 'antd'
+import { A } from 'src/components/atoms/A'
 
 export const MainHeader = () => {
   return (
@@ -12,11 +14,13 @@ export const MainHeader = () => {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
+            flexFlow: 'column',
             fontSize: '48px',
             lineHeight: '64px'
           }}
         >
           <p style={{ color: '#2F80ED' }}>Make a community sustainable together</p>
+          {A({ href: '/how-it-works' })(<Button type="primary">How it works</Button>)}
         </div>
       </div>
     </>
