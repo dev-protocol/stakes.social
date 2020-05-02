1a6ae8b0bb422e7d86fb1da543c5302833958e2e
aggre <hiroyuki.aggre@gmail.com>
2020-05-02 16:22:01 +0900

    implement calculation for average interest each month

--- packages/web/src/fixtures/utility/gql-hooks-wrapper.ts
+++ packages/web/src/fixtures/utility/gql-hooks-wrapper.ts
@@ -11,6 +11,11 @@ export const useAverageInterestRate = (metrics: string) => {
   const lockup = rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.lockup
     ? new BigNumber(rewardCalculationResultData?.reward_calculation_result_aggregate.aggregate?.sum?.lockup)
     : undefined
+  const _blocks = rewardCalculationResultData?.reward_calculation_result?.reduce(
+    (a, c) => a + c.allocation?.condition?.blocks,
+    0
+  )
+  const block = _blocks || 1
 
-  return stakingReward && lockup ? stakingReward.div(lockup).times(100) : new BigNumber(0)
+  return stakingReward && lockup ? stakingReward.div(block).times(172800).div(lockup).times(100) : new BigNumber(0)
 }
