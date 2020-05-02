a114e0a26b08e4d55ebe40c4fe549fbffd7d096b
k3nt0w <kento.watanabe.11@gmail.com>
2020-05-02 13:45:10 +0900

    feat: add meta tag

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -28,7 +28,7 @@ class NextApp extends App<AppInitialProps & WithApolloProps<{}>> {
       <>
         <Head>
           <title>boiler</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <Component {...pageProps} apollo={apollo} />
       </>
