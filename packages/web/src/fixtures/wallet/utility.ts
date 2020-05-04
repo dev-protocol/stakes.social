03eaecf658b8a93aec0dbdf735b05cdfc92789f1
aggre <hiroyuki.aggre@gmail.com>
2020-05-04 10:22:34 +0900

    fix method of getting an account address

--- packages/web/src/fixtures/wallet/utility.ts
+++ packages/web/src/fixtures/wallet/utility.ts
@@ -1,9 +1,20 @@
+import Web3 from 'web3'
+
+const cache: WeakMap<NonNullable<Window['ethereum']>, string> = new WeakMap()
+
 export const isAvailableWallet = () => (window?.ethereum ? true : false)
 
-export const getAccountAddress = () => {
+export const getAccountAddress = async () => {
   const { ethereum } = window
   if (ethereum) {
-    return ethereum.selectedAddress
+    return (async fromCache => {
+      if (typeof fromCache === 'string') {
+        return fromCache
+      }
+      const [account] = await new Web3(ethereum).eth.getAccounts()
+      cache.set(ethereum, account)
+      return account
+    })(cache.get(ethereum))
   }
   return undefined
 }
