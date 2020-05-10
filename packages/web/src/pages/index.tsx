ac2b167708ca92e2db5f32bd4cab8e3be0e59a92
k3nt0w <kento.watanabe.11@gmail.com>
2020-05-10 11:33:51 +0900

    feat: modal for craating new property

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -2,8 +2,11 @@ import React, { useMemo } from 'react'
 import { PropertyCardList } from 'src/components/organisms/PropertyCardList'
 import { MainHeader } from 'src/components/organisms/MainHeader'
 import { Footer } from 'src/components/organisms/Footer'
-import { Button } from 'antd'
+import { Button, Modal } from 'antd'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
 import { useRouter } from 'next/router'
+
+const { confirm } = Modal
 
 type InitialProps = {}
 type Props = {} & InitialProps
@@ -18,11 +21,23 @@ const Index = (_: Props) => {
     return 1
   }, [router])
 
+  const onClick = () => {
+    confirm({
+      title: 'Sample Confirm Modal',
+      icon: <ExclamationCircleOutlined />,
+      content: 'message message....',
+      onOk: () => {},
+      onCancel: () => {}
+    })
+  }
+
   return (
     <>
       <MainHeader />
       <div style={{ maxWidth: '1048px', marginRight: 'auto', marginLeft: 'auto' }}>
-        <Button style={{ display: 'block', margin: '32px 0 32px auto' }}>Create a new Property</Button>
+        <Button onClick={onClick} style={{ display: 'block', margin: '32px 0 32px auto' }}>
+          Create a new Property
+        </Button>
         <PropertyCardList currentPage={page} />
       </div>
       <Footer />
