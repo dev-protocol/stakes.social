b877c51e15759db6002154980ba3dbff87315c5c
k3nt0w <kento.watanabe.11@gmail.com>
2020-05-13 10:41:40 +0900

    feat: auth page

---
+++ packages/web/src/pages/auth.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer } from 'src/components/organisms/Footer'
+
+type Props = {}
+
+const Auth = (_: Props) => {
+  return (
+    <>
+      <div>Auth</div>
+      <Footer />
+    </>
+  )
+}
+
+export default Auth
