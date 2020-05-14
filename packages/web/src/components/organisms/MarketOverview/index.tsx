402b2532e25fbb2d53a7ae1ae3035a148f5f3838
k3nt0w <kento.watanabe.11@gmail.com>
2020-05-15 08:52:50 +0900

    feat: get market address

--- packages/web/src/components/organisms/MarketOverview/index.tsx
+++ packages/web/src/components/organisms/MarketOverview/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Row, Col } from 'antd'
 import { TextCard } from 'src/components/molecules/TextCard'
-
+import { useGetMarketFactoryCreateQuery } from '@dev/graphql'
 export const MarketOverview = () => {
+  const { data } = useGetMarketFactoryCreateQuery()
+  console.log(data?.market_factory_create[0]?.market)
   return (
     <div style={{ maxWidth: '680px', marginRight: 'auto', marginLeft: 'auto' }}>
       <Row style={{ margin: '82px 0px' }}>
