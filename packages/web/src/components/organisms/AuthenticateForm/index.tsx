1f5ad707d6f3337209d441101a5a841c420c631f
k3nt0w <kento.watanabe.11@gmail.com>
2020-05-17 20:47:47 +0900

    feat: default value for development

--- packages/web/src/components/organisms/AuthenticateForm/index.tsx
+++ packages/web/src/components/organisms/AuthenticateForm/index.tsx
@@ -19,7 +19,8 @@ export const AuthenticateForm = ({ market }: Props) => {
   const onSearch = (query: string) => setQuery(query)
   useEffectAsync(async () => {
     const accountAddress = await getAccountAddress()
-    setAuthor(accountAddress || '')
+    const dafaultValue = process.env.NODE_ENV === 'development' ? '0x69Cc2C86aeB26f52F6645a2DFdec1051DD5584C0' : ''
+    setAuthor(accountAddress || dafaultValue)
   }, [])
   return (
     <div style={{ maxWidth: '680px', marginRight: 'auto', marginLeft: 'auto' }}>
