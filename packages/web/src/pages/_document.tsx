33b32b0ac014ff34824c9c8a7870a6a4888fe82e
aggre <hiroyuki.aggre@gmail.com>
2020-05-19 17:15:43 +0900

    change files path to //asset.stakes.social

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -31,22 +31,22 @@ export default class extends Document {
           {/* Use minimum-scale=1 to enable GPU rasterization */}
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" />
-          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-          <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" sizes="57x57" href="//asset.stakes.social/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="//asset.stakes.social/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="//asset.stakes.social/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="//asset.stakes.social/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="//asset.stakes.social/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="//asset.stakes.social/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="//asset.stakes.social/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="//asset.stakes.social/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="//asset.stakes.social/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="//asset.stakes.social/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="//asset.stakes.social/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="//asset.stakes.social/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="//asset.stakes.social/favicon-16x16.png" />
+          <link rel="manifest" href="//asset.stakes.social/manifest.json" />
           <meta name="msapplication-TileColor" content="#2F80ED" />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="msapplication-TileImage" content="//asset.stakes.social/ms-icon-144x144.png" />
           <meta name="theme-color" content="#2F80ED" />
         </Head>
         <body>
