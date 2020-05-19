d9d42a3546e3c6baf582f918ed580be13c646657
k3nt0w <kento.watanabe.11@gmail.com>
2020-05-19 09:18:13 +0900

    feat: add text component

--- packages/web/src/components/atoms/Typography/index.tsx
+++ packages/web/src/components/atoms/Typography/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Typography } from 'antd'
+import styled from 'styled-components'
 
-const { Title } = Typography
+const { Title, Text } = Typography
 
 export const H1: React.FC = ({ children }) => (
   <Title level={1} style={{ color: '#2F80ED' }}>
@@ -9,3 +10,13 @@ export const H1: React.FC = ({ children }) => (
   </Title>
 )
 export const H2: React.FC = ({ children }) => <Title level={2}>{children}</Title>
+
+export const H3: React.FC = ({ children }) => <Title level={3}>{children}</Title>
+
+export const H4: React.FC = ({ children }) => <Title level={4}>{children}</Title>
+
+export const Body1 = styled(Text)`
+  font-size: 14px;
+  line-height: 22px;
+  color: #000;
+`
