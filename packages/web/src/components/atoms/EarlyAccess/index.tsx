9bdd30d220ddc0bca2c8df84f1736e6fd77545e7
aggre <hiroyuki.aggre@gmail.com>
2020-06-15 02:14:25 +0900

    add new feature notice

--- packages/web/src/components/atoms/EarlyAccess/index.tsx
+++ packages/web/src/components/atoms/EarlyAccess/index.tsx
@@ -8,9 +8,26 @@ const Wrap = styled.div`
   color: white;
   text-align: center;
 `
+const Feature = styled(Wrap)`
+  background: #333;
+  a {
+    color: white;
+    text-decoration: overline;
+  }
+`
 
 export const EarlyAccess = () => (
-  <Wrap>
-    <span>🧪Early Access Version🧬</span>
-  </Wrap>
+  <>
+    <Wrap>
+      <span>🧪Early Access Version🧬</span>
+    </Wrap>
+    <Feature>
+      <span>
+        Next:{' '}
+        <a href="//github.com/dev-protocol/DIPs/issues/4#issuecomment-641076014" target="_blank" rel="noreferrer">
+          DIP4 is rolling out soon
+        </a>
+      </span>
+    </Feature>
+  </>
 )
