dd54a2ad236909f63e689e880c20b496fee6ee83
k3nt0w <kento.watanabe.11@gmail.com>
2020-06-28 19:40:34 +0900

    refactor: handle undefine

--- packages/web/src/fixtures/dev-kit/hooks.ts
+++ packages/web/src/fixtures/dev-kit/hooks.ts
@@ -201,7 +201,7 @@ export const useGetWithdrawalStatus = (propertyAddress: string) => {
   )
   const withdrawable = getBlockNumber()?.then(x => (x && data ? x >= data : false))
 
-  return { withdrawalStatus: data ? data : undefined, withdrawable, error }
+  return { withdrawalStatus: data, withdrawable, error }
 }
 
 export const useAssetStrength = (metricsAddress: string, marketAddress: string) => {
