b28129c05cda080aebc1aba143566e8071e781be
hmsk <k.hamasaki@gmail.com>
2020-07-18 19:00:43 -0700

    Format locale data JSON with Prettier
    Avoid generating diffs per running "@dev/i18n build"
    (However, diffs disappear by running commit hook)

--- packages/i18n/src/locales/run.ts
+++ packages/i18n/src/locales/run.ts
@@ -1,10 +1,11 @@
 import { promises as fs } from 'fs'
+import Prettier from 'prettier'
 import { commonTranslationEN, commonTranslationJA } from './Common'
 
 const basePath = `${__dirname}/../../../web/public/locales`
 
 export const makeLocaleJson = async (filePath: string, data: any) =>
-  fs.writeFile(`${basePath}/${filePath}`, JSON.stringify(data))
+  fs.writeFile(`${basePath}/${filePath}`, Prettier.format(JSON.stringify(data), { parser: 'json' }))
 
 const run = async () => {
   await makeLocaleJson('en/Common.json', commonTranslationEN)
