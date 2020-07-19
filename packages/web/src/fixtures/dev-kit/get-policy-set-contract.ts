7b3a7f531b4cc89a4c865aac0dd2851ba62aa743
k3nt0w <kento.watanabe.11@gmail.com>
2020-07-19 15:40:57 +0900

    fix: remove useless file

--- packages/web/src/fixtures/dev-kit/get-policy-set-contract.ts
+++
@@ -1,8 +0,0 @@
-import Web3 from 'web3'
-// import { createPolicySetContract } from '@devprtcl/dev-kit-js/esm/policy-set'
-// import { CustomOptions } from '@devprtcl/dev-kit-js/esm/option'
-
-// eslint-disable-next-line
-export const getPolicySetContract = (client: Web3, address?: string, options?: any) => {}
-// FIXME(@k3nt0w): this is test code for confirmation operation.
-// createPolicySetContract(client)(address, options)
