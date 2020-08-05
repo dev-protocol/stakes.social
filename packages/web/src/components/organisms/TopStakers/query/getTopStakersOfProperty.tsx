56338f798f1f38fac3c12fd78218ed599cd5e2fa
Cold-A-Muse <daan.998@hotmail.com>
2020-08-05 11:27:22 +0200

    Initial commit

---
+++ packages/web/src/components/organisms/TopStakers/query/getTopStakersOfProperty.tsx
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag'
+
+export const getTopStakersOfPropertyQuery = gql`
+  query getTopStakersOfProperty($property_address: String!, $limit: Int!) {
+    property_lockup(where: { property_address: { _eq: $property_address } }, order_by: { value: desc }, limit: $limit) {
+      account_address
+      value
+    }
+  }
+`
+
+export default getTopStakersOfPropertyQuery
