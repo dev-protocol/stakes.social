90038743cb73e7c34b19d4910d62f503fce15a2e
Hideo Hattori <hhatto.jp@gmail.com>
2020-08-24 22:56:17 +0900

    switch Khaos URLs for mainnet and ropsten by NODE_ENV

--- packages/web/src/fixtures/khaos/cache-path.ts
+++ packages/web/src/fixtures/khaos/cache-path.ts
@@ -1,4 +1,7 @@
-export const BaseUrl = 'https://khaos-eth-mainnet.azurewebsites.net'
+export const BaseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://khaos-eth-mainnet.azurewebsites.net'
+    : 'https://khaos-eth-ropsten.azurewebsites.net'
 
 export const SWRCachePath = {
   postSignGitHubMarketAsset: () => `${BaseUrl}/sign/github-market`
