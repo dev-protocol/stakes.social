9d672bba179a7c4f079042b7c2aa4684d4d166b9
Hideo Hattori <hhatto.jp@gmail.com>
2020-09-11 22:41:21 +0900

    add MyStakingProperties component to Account page

--- packages/web/src/pages/settings/profile.tsx
+++ packages/web/src/pages/settings/profile.tsx
@@ -4,6 +4,7 @@ import { Header } from 'src/components/organisms/Header'
 import { Headline } from 'src/components/atoms/Headline'
 import { H2 } from 'src/components/atoms/Typography'
 import { UserProfile } from 'src/components/organisms/UserProfile'
+import { MyStakingProperties } from 'src/components/organisms/MyStakingProperties'
 
 type Props = {}
 
@@ -11,6 +12,10 @@ const SettingsProfile = (_: Props) => {
   return (
     <>
       <Header />
+      <Headline height={300}>
+        <H2>My Staking Properties</H2>
+      </Headline>
+      <MyStakingProperties />
       <Headline height={300}>
         <H2>Profile Settings</H2>
       </Headline>
