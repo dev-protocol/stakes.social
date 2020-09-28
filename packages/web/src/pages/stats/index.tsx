110725b1c2edc51d6768493948da79ae2c8a9135
Hideo Hattori <hhatto.jp@gmail.com>
2020-09-28 23:39:40 +0900

    componentize Buy Dev Button

--- packages/web/src/pages/stats/index.tsx
+++ packages/web/src/pages/stats/index.tsx
@@ -5,23 +5,9 @@ import { Header } from 'src/components/organisms/Header'
 import { DevStats } from 'src/components/organisms/DevStats'
 import { H2 } from 'src/components/atoms/Typography'
 import { Headline } from 'src/components/atoms/Headline'
-import styled from 'styled-components'
+import { BuyDevButton } from 'src/components/molecules/BuyButton'
 
 type Props = {}
-
-const BuyButton = styled.button<{ bgColor?: string }>`
-  padding: 6px 24px;
-  border-radius: 9px;
-  border: none;
-  background-color: #2f80ed;
-  color: white;
-
-  cursor: pointer;
-  :hover {
-    transition: ease-in-out 0.2s;
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-  }
-`
 
 const DevProtocolStats = (_: Props) => {
   return (
@@ -32,7 +18,7 @@ const DevProtocolStats = (_: Props) => {
         <Headline height={200}>
           <H2>Dev Protocol Stats</H2>
           <p>Dev Protocol is being adopted, see for yourself.</p>
-          <BuyButton>Buy DEV</BuyButton>
+          <BuyDevButton />
         </Headline>
         <DevStats />
       </div>
