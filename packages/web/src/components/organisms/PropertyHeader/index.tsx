f12b819db2274b4ffdd8f1a4fab7846226cd5c90
Cold-A-Muse <daan.998@hotmail.com>
2020-10-08 11:26:15 +0200

    Hhato feedback

--- packages/web/src/components/organisms/PropertyHeader/index.tsx
+++ packages/web/src/components/organisms/PropertyHeader/index.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { useGetPropertyAuthenticationQuery } from '@dev/graphql'
 import BigNumber from 'bignumber.js'
 import { BuyDevButton } from 'src/components/molecules/BuyButton'
+import { Divider } from 'antd'
 
 const ResponsivePropertyAddressFrame = styled.div`
   margin: 2rem auto;
@@ -44,7 +45,8 @@ export const PropertyHeader = ({ propertyAddress, apy, creators }: Props) => {
       </div>
       <SubHeader>
         <p style={{ marginBottom: 0 }}>
-          <span style={{ color: '#1AC9FC' }}>{apy?.dp(0).toNumber() || 'N/A'}%</span> APY Stakers |{' '}
+          <span style={{ color: '#1AC9FC' }}>{apy?.dp(0).toNumber() || 'N/A'}%</span> APY Stakers
+          <Divider type="vertical" style={{ borderLeft: '1px solid black' }} />
           <span style={{ color: '#1AC9FC' }}> {creators?.dp(0).toNumber() || 'N/A'}%</span> APY Creators
         </p>
         <BuyDevButton>Buy DEV</BuyDevButton>
