a8a1e78f7476c9787c7d79d22627f3875e4a8c24
aggre <hiroyuki.aggre@gmail.com>
2020-11-09 22:34:43 +0900

    use `useProvider`

--- packages/web/src/fixtures/_pages/liquidity/uniswap-pool/client.ts
+++ packages/web/src/fixtures/_pages/liquidity/uniswap-pool/client.ts
@@ -7,69 +7,63 @@ import { toEVMBigNumber } from 'src/fixtures/utility'
 import BigNumber from 'bignumber.js'
 import { utils } from '@devprtcl/dev-kit-js'
 import { ETHDEV_V2_ADDRESS } from '../constants/address'
-import { WEB3_PROVIDER_ENDPOINT } from 'src/fixtures/wallet/constants'
 
 const { execute } = utils
 const client: Map<string, Contract> = new Map()
 
-export const getClient = (contractAddress = ETHDEV_V2_ADDRESS): [] | [Contract, Web3] => {
-  const web3 = new Web3(WEB3_PROVIDER_ENDPOINT)
-  if (!web3) {
-    return []
-  }
-
+export const getContract = (web3: Web3, contractAddress = ETHDEV_V2_ADDRESS): Contract => {
   const stored = client.get(contractAddress)
   if (stored) {
-    return [stored, web3]
+    return stored
   }
 
   const contract = (createContract(abi, contractAddress, web3) as unknown) as Contract
   client.set(contractAddress, contract)
 
-  return [contract, web3]
+  return contract
 }
 
-export const balanceOf = async (web3?: Web3): Promise<BigNumber | undefined> => {
+export const balanceOf = async (web3: Web3): Promise<BigNumber | undefined> => {
   const address = await getAccountAddress(web3)
   if (address === undefined) {
     return undefined
   }
-  return (([contract]) =>
+  return (contract =>
     contract
       ? execute({
           contract,
           method: 'balanceOf',
           args: [address]
         })
-      : Promise.resolve(''))(getClient()).then(toEVMBigNumber)
+      : Promise.resolve(''))(getContract(web3)).then(toEVMBigNumber)
 }
 
-export const allowance = async (spender: string, web3?: Web3): Promise<BigNumber | undefined> => {
+export const allowance = async (web3: Web3, spender: string): Promise<BigNumber | undefined> => {
   const address = await getAccountAddress(web3)
   if (address === undefined) {
     return undefined
   }
-  return (([contract]) =>
+  return (contract =>
     contract
       ? execute({
           contract,
           method: 'allowance',
           args: [address, spender]
         })
-      : Promise.resolve(''))(getClient()).then(toEVMBigNumber)
+      : Promise.resolve(''))(getContract(web3)).then(toEVMBigNumber)
 }
 
-export const approve = async (spender: string, value: BigNumber) => {
-  return (([contract, client]) =>
+export const approve = async (web3: Web3, spender: string, value: BigNumber) => {
+  return (contract =>
     contract && client
       ? execute({
           contract,
-          client,
+          client: web3,
           mutation: true,
           method: 'approve',
           args: [spender, value.toFixed()]
         })
-      : Promise.resolve())(getClient())
+      : Promise.resolve())(getContract(web3))
 }
 
 export const fromTheGraph = async (): Promise<{
