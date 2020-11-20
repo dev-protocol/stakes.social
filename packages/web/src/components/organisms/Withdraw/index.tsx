55e4838acb24e6f7536455ee283056e24536ced7
aggre <hiroyuki.aggre@gmail.com>
2020-11-20 15:14:33 +0900

    delete estimate section

--- packages/web/src/components/organisms/Withdraw/index.tsx
+++ packages/web/src/components/organisms/Withdraw/index.tsx
@@ -1,36 +1,20 @@
-import React, { useCallback, useEffect, useState, useMemo, ChangeEvent } from 'react'
+import React, { useCallback, useState, useMemo, ChangeEvent, useEffect } from 'react'
 import { useProvider } from 'src/fixtures/wallet/hooks'
 import { getMyStakingAmount } from 'src/fixtures/dev-kit/client'
-import { useWithdrawStaking, useGetMyStakingAmount, useGetMyStakingRewardAmount } from 'src/fixtures/dev-kit/hooks'
-import { toAmountNumber, toBigNumber, toNaturalNumber, whenDefined, whenDefinedAll } from 'src/fixtures/utility'
+import { useWithdrawStaking } from 'src/fixtures/dev-kit/hooks'
+import { toAmountNumber, toNaturalNumber, whenDefinedAll } from 'src/fixtures/utility'
 import { TransactForm } from 'src/components/molecules/TransactForm'
-import styled from 'styled-components'
-import { Switch } from 'antd'
 import { FormContainer } from 'src/components/molecules/TransactForm/FormContainer'
-import { Estimated } from 'src/components/molecules/TransactForm/Estimated'
 
 interface Props {
   className?: string
   title?: string
   propertyAddress: string
+  onChange?: (value: string) => void
 }
 
-const Wrap = styled.div`
-  display: grid;
-  gap: 0.5rem;
-  justify-content: stretch;
-  align-items: center;
-  grid-template-columns: auto 1fr;
-`
-
-export const Withdraw = ({ className, title, propertyAddress }: Props) => {
-  const [withdrawBoth, setWithdrawBoth] = useState<boolean>(false)
+export const Withdraw = ({ className, title, propertyAddress, onChange: onChangeAmount }: Props) => {
   const [withdrawAmount, setWithdrawAmount] = useState<string>('')
-  const [claimedTokens, setClaimedTokens] = useState<string | undefined>()
-  const [interestTokens, setInterestTokens] = useState<string | undefined>()
-  const [withdrawableTokens, setWithdrawableTokens] = useState<string>('')
-  const { myStakingRewardAmount } = useGetMyStakingRewardAmount(propertyAddress)
-  const { myStakingAmount } = useGetMyStakingAmount(propertyAddress)
   const { web3, accountAddress } = useProvider()
   const { withdrawStaking } = useWithdrawStaking()
   const withdrawFor = useCallback(
@@ -39,69 +23,35 @@ export const Withdraw = ({ className, title, propertyAddress }: Props) => {
     },
     [withdrawStaking, propertyAddress]
   )
-  const onChangeSwitch = (checked: boolean) => {
-    setWithdrawBoth(checked)
-    setWithdrawAmount('')
-  }
   const onClickMax = () =>
     whenDefinedAll([web3, accountAddress], ([libWeb3, account]) =>
       getMyStakingAmount(libWeb3, propertyAddress, account)
         .then(async x => toNaturalNumber(x))
         .then(x => setWithdrawAmount(x.toFixed()))
     )
-  const onChange = useMemo(
-    () =>
-      withdrawBoth
-        ? (event: ChangeEvent<HTMLInputElement>) => {
-            setWithdrawAmount(event.target.value)
-          }
-        : undefined,
-    [withdrawBoth, setWithdrawAmount]
-  )
-
-  const estimatedTitle = useMemo(() => (withdrawBoth ? 'Staked and Reward Amount' : 'Withdrawable Reward Amount'), [
-    withdrawBoth
-  ])
-  const estimatedValue = useMemo(
-    () =>
-      withdrawBoth ? (
-        <p>
-          {withdrawableTokens || 0} DEV{' '}
-          {claimedTokens && interestTokens ? `(${claimedTokens} + ${interestTokens})` : ''}
-        </p>
-      ) : (
-        <p>{withdrawableTokens || 0} DEV</p>
-      ),
-    [withdrawBoth, withdrawableTokens, claimedTokens, interestTokens]
-  )
-  const Label = useMemo(() => (title ? () => <label htmlFor="withdraw">{title}</label> : () => <></>), [title])
-
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setWithdrawAmount(event.target.value)
+  }
   useEffect(() => {
-    const reward = myStakingRewardAmount
-    const staking = whenDefined(myStakingAmount, x =>
-      x.isGreaterThan(withdrawAmount || 0) ? toBigNumber(withdrawAmount) : x
-    )
-    setClaimedTokens(whenDefined(staking, x => x.dp(2).toFixed()))
-    setInterestTokens(whenDefined(reward, x => x.dp(2).toFixed()))
-    setWithdrawableTokens(staking && reward ? staking.plus(reward).dp(2).toFixed() : '0')
-  }, [myStakingRewardAmount, myStakingAmount, withdrawAmount, setClaimedTokens])
+    if (onChangeAmount) {
+      onChangeAmount(withdrawAmount)
+    }
+  }, [withdrawAmount, onChangeAmount])
+
+  const Label = useMemo(() => (title ? () => <label htmlFor="withdraw">{title}</label> : () => <></>), [title])
 
   return (
     <FormContainer>
       <Label />
-      <Wrap>
-        <Switch unCheckedChildren="Stake" onChange={onChangeSwitch} />
-        <TransactForm
-          className={className}
-          id="withdraw"
-          enterButton="Withdraw"
-          value={withdrawAmount}
-          onChange={onChange}
-          onSearch={withdrawFor}
-          onClickMax={onClickMax}
-        ></TransactForm>
-      </Wrap>
-      <Estimated title={estimatedTitle}>{estimatedValue}</Estimated>
+      <TransactForm
+        className={className}
+        id="withdraw"
+        enterButton="Withdraw"
+        value={withdrawAmount}
+        onChange={onChange}
+        onSearch={withdrawFor}
+        onClickMax={onClickMax}
+      ></TransactForm>
     </FormContainer>
   )
 }
