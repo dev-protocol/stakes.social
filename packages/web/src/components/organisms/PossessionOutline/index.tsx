633843574b9d0b9a61362ff0d65d751654c02219
aggre <hiroyuki.aggre@gmail.com>
2020-11-20 15:42:49 +0900

    add the withdrawable staking reward amount section

--- packages/web/src/components/organisms/PossessionOutline/index.tsx
+++ packages/web/src/components/organisms/PossessionOutline/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { useGetTotalStakingAmount, useGetMyStakingAmount, useGetTotalRewardsAmount } from 'src/fixtures/dev-kit/hooks'
+import {
+  useGetTotalStakingAmount,
+  useGetMyStakingAmount,
+  useGetTotalRewardsAmount,
+  useGetMyStakingRewardAmount
+} from 'src/fixtures/dev-kit/hooks'
 import styled from 'styled-components'
-import { Statistic } from 'antd'
+import { Card, Statistic } from 'antd'
 
 interface Props {
   className?: string
@@ -10,12 +15,18 @@ interface Props {
 
 const Wrap = styled.div`
   display: grid;
-  grid-gap: 1rem;
+  gap: 1rem;
   grid-template-columns: 1fr 1fr;
-  justify-content: space-between;
+  justify-content: stretch;
+  & > *:last-child {
+    grid-column: 1 / 3;
+  }
   @media (min-width: 1120px) {
     grid-auto-flow: column;
     grid-template-columns: auto;
+    & > *:last-child {
+      grid-column: auto;
+    }
   }
 `
 
@@ -23,35 +34,54 @@ export const PossessionOutline = ({ className, propertyAddress }: Props) => {
   const { totalStakingAmount, currency: totalStakingAmountCurrency } = useGetTotalStakingAmount(propertyAddress)
   const { totalRewardsAmount, currency: totalRewardsAmountCurrency } = useGetTotalRewardsAmount(propertyAddress)
   const { myStakingAmount, currency: myStakingAmountCurrency } = useGetMyStakingAmount(propertyAddress)
+  const { myStakingRewardAmount, currency: myStakingRewardAmountCurrency } = useGetMyStakingRewardAmount(
+    propertyAddress
+  )
 
   return (
     <Wrap className={className}>
-      <Statistic
-        title="Total Staking Amount"
-        value={totalStakingAmount ? totalStakingAmount.toNumber() : 'N/A'}
-        precision={2}
-        suffix={totalStakingAmountCurrency}
-      />
-      <Statistic
-        title="Your Staking Amount"
-        value={myStakingAmount ? myStakingAmount.toNumber() : 'N/A'}
-        precision={2}
-        suffix={myStakingAmountCurrency}
-      />
-      <Statistic
-        title="Your Staking Share"
-        value={
-          myStakingAmount && totalStakingAmount && (myStakingAmount.toNumber() / totalStakingAmount.toNumber()) * 100
-        }
-        precision={2}
-        suffix="%"
-      />
-      <Statistic
-        title="Total Rewards"
-        value={totalRewardsAmount && totalRewardsAmount.toNumber()}
-        precision={2}
-        suffix={totalRewardsAmountCurrency}
-      />
+      <Card>
+        <Statistic
+          title="Total Staking Amount"
+          value={totalStakingAmount ? totalStakingAmount.toNumber() : 'N/A'}
+          precision={2}
+          suffix={totalStakingAmountCurrency}
+        />
+      </Card>
+      <Card>
+        <Statistic
+          title="Total Rewards"
+          value={totalRewardsAmount && totalRewardsAmount.toNumber()}
+          precision={2}
+          suffix={totalRewardsAmountCurrency}
+        />
+      </Card>
+      <Card>
+        <Statistic
+          title="Your Staking Share"
+          value={
+            myStakingAmount && totalStakingAmount && (myStakingAmount.toNumber() / totalStakingAmount.toNumber()) * 100
+          }
+          precision={2}
+          suffix="%"
+        />
+      </Card>
+      <Card>
+        <Statistic
+          title="Your Staking Amount"
+          value={myStakingAmount ? myStakingAmount.toNumber() : 'N/A'}
+          precision={2}
+          suffix={myStakingAmountCurrency}
+        />
+      </Card>
+      <Card>
+        <Statistic
+          title="Your Rewards"
+          value={myStakingRewardAmount && myStakingRewardAmount.toNumber()}
+          precision={2}
+          suffix={myStakingRewardAmountCurrency}
+        />
+      </Card>
     </Wrap>
   )
 }
