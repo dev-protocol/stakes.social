a184f46b79cbc6117351bc00b350377451904fe9
Hideo Hattori <hhatto.jp@gmail.com>
2020-12-01 23:10:36 +0900

    fix: dynamic property cover image

--- packages/web/src/components/atoms/CoverImageOrGradient/index.tsx
+++ packages/web/src/components/atoms/CoverImageOrGradient/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
 import styled from 'styled-components'
 
-type Props = { src?: string; ratio?: number; className?: string }
+interface Props {
+  src?: string
+  ratio?: number
+}
 
 const Base = styled.div`
   position: relative;
   background-size: cover;
   background-position: center;
 `
-const Cover = ({ src, ratio = 52.5 }: Props) => styled(Base)`
-  padding-top: ${ratio}%;
-  background-image: url(${src}),
+
+export const CoverImageOrGradient = styled(Base)<Props>`
+  padding-top: ${({ ratio }) => ratio}%;
+  background-image: url(${({ src }) => src}),
     linear-gradient(134deg, rgb(47, 67, 237) 0%, rgb(47, 128, 237) 23%, rgb(47, 172, 237) 46%, rgb(190, 208, 230) 100%);
 `
-
-export const CoverImageOrGradient = (props: Props) => (C => <C className={props.className} />)(Cover(props))
