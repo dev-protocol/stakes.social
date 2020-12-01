230c5737378cb1024326fdf0709eba63838e021c
aggre <hiroyuki.aggre@gmail.com>
2020-12-01 11:15:57 +0900

    rerun svgr

--- packages/web/src/components/atoms/Svgs/tsx/index.tsx
+++ packages/web/src/components/atoms/Svgs/tsx/index.tsx
@@ -1,5 +1,3 @@
 export { default as Camp } from './Camp'
 export { default as Logo } from './Logo'
-export { default as StakesSocialBlack } from './StakesSocialBlack'
-export { default as StakesSocialWhite } from './StakesSocialWhite'
-export { default as Hamburger } from './Hamburger'
+export { default as StakesSocial } from './StakesSocial'
