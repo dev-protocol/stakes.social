6e61350043c4f0ebc435df25a6ca5fe9d1d85f1b
Hideo Hattori <hhatto.jp@gmail.com>
2020-12-08 22:37:55 +0900

    add unit test for dev-for-apps fixtures

--- packages/web/src/fixtures/dev-for-apps/functions/useUploadFile.spec.ts
+++ packages/web/src/fixtures/dev-for-apps/functions/useUploadFile.spec.ts
@@ -1,7 +1,7 @@
 import useSWR from 'swr'
 import { renderHook, act } from '@testing-library/react-hooks'
-import { useUploadFile } from './useUploadFile'
-import { postUploadFile } from '../utility'
+import { useUploadFile, useDeleteFile } from './useUploadFile'
+import { postUploadFile, deleteUploadFile } from '../utility'
 import { signWithCache } from 'src/fixtures/wallet/utility'
 
 jest.mock('swr')
@@ -56,5 +56,54 @@ describe('useUploadFile', () => {
     expect(result.current.isLoading).toBe(false)
     expect(v).toBe(undefined)
     expect((postUploadFile as jest.Mock).mock.calls.length).toBe(0)
+  })
+})
+
+describe('useDeleteFile', () => {
+  test('success delete file', async () => {
+    ;(useSWR as jest.Mock).mockImplementation(() => ({ mutate: () => {} }))
+    ;(deleteUploadFile as jest.Mock).mockResolvedValue({ id: 'id' })
+    ;(signWithCache as jest.Mock).mockImplementation(() => ({
+      signature: 'test signature',
+      message: 'test sign message'
+    }))
+    const { result, waitForNextUpdate } = renderHook(() => useDeleteFile('0x01234567890'))
+    act(() => {
+      result.current.deleteFileHandler(1, 'dummy-filename.jpg')
+    })
+    await waitForNextUpdate()
+    expect(result.current.isLoading).toBe(false)
+  })
+
+  test('failure delete file', async () => {
+    const errorMessage = 'error'
+    const error = new Error(errorMessage)
+    ;(useSWR as jest.Mock).mockImplementation(() => ({ mutate: () => {} }))
+    ;(deleteUploadFile as jest.Mock).mockRejectedValue(error)
+    ;(signWithCache as jest.Mock).mockImplementation(() => ({
+      signature: 'test signature',
+      message: 'test sign message'
+    }))
+    const { result, waitForNextUpdate } = renderHook(() => useDeleteFile('0x01234567890'))
+    act(() => {
+      result.current.deleteFileHandler(1, 'dummy-filename.jpg')
+    })
+    await waitForNextUpdate()
+    expect(result.current.isLoading).toBe(false)
+    expect(result.current.error?.message).toBe(errorMessage)
+    expect((deleteUploadFile as jest.Mock).mock.calls.length).toBe(1)
+  })
+
+  test('failure delete file with web3.sign error', async () => {
+    const errorMessage = 'error'
+    const error = new Error(errorMessage)
+    ;(useSWR as jest.Mock).mockImplementation(() => ({ mutate: () => {} }))
+    ;(deleteUploadFile as jest.Mock).mockRejectedValue(error)
+    ;(signWithCache as jest.Mock).mockImplementation(() => ({ signature: undefined, message: undefined }))
+    const { result } = renderHook(() => useDeleteFile('0x01234567890'))
+    const v = await result.current.deleteFileHandler(1, 'dummy-filename.jpg')
+    expect(result.current.isLoading).toBe(false)
+    expect(v).toBe(undefined)
+    expect((deleteUploadFile as jest.Mock).mock.calls.length).toBe(0)
   })
 })
