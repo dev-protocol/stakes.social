dfe0aadaa1f345c8eaea6479dcdc5599858e257c
Hideo Hattori <hhatto.jp@gmail.com>
2021-01-08 21:42:39 +0900

    refactoring

--- packages/web/src/fixtures/ens/hooks.ts
+++ packages/web/src/fixtures/ens/hooks.ts
@@ -2,11 +2,9 @@ import { ethers } from 'ethers'
 import { WEB3_PROVIDER_ENDPOINT } from 'src/fixtures/wallet/constants'
 
 export const useENS = () => {
-  const provider = new ethers.providers.JsonRpcProvider(WEB3_PROVIDER_ENDPOINT)
   const getENS = async (address: string) => {
-    return provider.lookupAddress(address).then(result => {
-      return result
-    })
+    const provider = new ethers.providers.JsonRpcProvider(WEB3_PROVIDER_ENDPOINT)
+    return provider.lookupAddress(address)
   }
 
   return { getENS }
