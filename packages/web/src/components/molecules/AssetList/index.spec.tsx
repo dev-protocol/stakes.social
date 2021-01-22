6023ad580f3be67770916bfa717f2dda4f238071
Hideo Hattori <hhatto.jp@gmail.com>
2021-01-22 22:08:32 +0900

    add more unit tests (#946)
    * no skipping for unit test of PropertyCardList component
    
    * fix wording
    
    * add unit test for AssetList and NotConnectedAndEmpty components
    
    * remove unnecessary props of component on snapshot testing
    
    * update test for PropertyTags component
    
    * add github fixtures test
    
    * add unit test for Stake component events
    
    * add unit test for hooks on liquidity Deposit component
    
    * add unit test for hooks on CancelStaking component
    
    * add unit test for Withdraw component events

--- packages/web/src/components/molecules/AssetList/index.spec.tsx
+++ packages/web/src/components/molecules/AssetList/index.spec.tsx
@@ -2,15 +2,28 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { AssetList } from '.'
 import 'src/__mocks__/window/matchMedia.mock'
+import { useProvider } from 'src/fixtures/wallet/hooks'
 
 jest.mock('src/fixtures/dev-kit/hooks')
 jest.mock('src/fixtures/dev-for-apps/hooks')
+jest.mock('src/fixtures/wallet/hooks')
 
 describe(`${AssetList.name}`, () => {
   test('Snapshot', () => {
     const component = render(
       <AssetList properties={['1', '2', '3']} onPagination={() => {}} enableStake={true} total={6} />
     )
+    const tree = component.baseElement
+    expect(tree).toMatchSnapshot()
+  })
+  test('Snapshot with empty properties', () => {
+    const component = render(<AssetList properties={[]} onPagination={() => {}} enableStake={true} total={6} />)
+    const tree = component.baseElement
+    expect(tree).toMatchSnapshot()
+  })
+  test('Snapshot with empty properties and sign in button', () => {
+    ;(useProvider as jest.Mock).mockReturnValue(() => ({ accountAddress: '' }))
+    const component = render(<AssetList properties={[]} onPagination={() => {}} enableStake={true} total={6} />)
     const tree = component.baseElement
     expect(tree).toMatchSnapshot()
   })
--- packages/web/src/fixtures/wallet/__mocks__/hooks.ts
+++ packages/web/src/fixtures/wallet/__mocks__/hooks.ts
@@ -1,5 +1,9 @@
 import Web3 from 'web3'
 
-export const useProvider = () => {
+export const useConnectWallet = jest.fn(() => {
+  return { isConnected: true, connect: () => {}, isConnecting: false }
+})
+
+export const useProvider = jest.fn(() => {
   return { web3: new Web3(), nonConnectedWeb3: new Web3(), accountAddress: '0x' }
-}
+})
