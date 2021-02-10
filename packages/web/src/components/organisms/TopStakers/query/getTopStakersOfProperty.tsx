ce4e37a208ef95e32551eb4373aac74c544247d9
Hideo Hattori <hhatto.jp@gmail.com>
2021-02-10 01:32:12 -0900

    Use @apollo/client's gql (#986)
    * use @apollo/client's gql
    
    * update graphql code

--- packages/web/src/components/organisms/TopStakers/query/getTopStakersOfProperty.tsx
+++ packages/web/src/components/organisms/TopStakers/query/getTopStakersOfProperty.tsx
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag'
+import { gql } from '@apollo/client'
 
 export const getTopStakersOfPropertyQuery = gql`
   query getTopStakersOfProperty($property_address: String!, $limit: Int!) {
