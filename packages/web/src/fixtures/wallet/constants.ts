41cfb164927437d47955a0b4d7662abccf097b5b
aggre <hiroyuki.aggre@gmail.com>
2021-03-02 12:50:22 +0900

    change priority

--- packages/web/src/fixtures/wallet/constants.ts
+++ packages/web/src/fixtures/wallet/constants.ts
@@ -1,6 +1,6 @@
 export const WEB3_PROVIDER_ENDPOINT =
-  process.env.NODE_ENV === 'production'
-    ? 'https://eth-mainnet.alchemyapi.io/v2/0EvZQA7WvDYf40cz476eEIh348_PcZJu' // For Production build
-    : process.env.VERCEL_ENV === 'preview'
+  process.env.VERCEL_ENV === 'preview'
     ? 'https://eth-mainnet.alchemyapi.io/v2/ZeWhYPhzncyM-pDIyX-WuifD1Iosc06s' // For Staging
+    : process.env.NODE_ENV === 'production'
+    ? 'https://eth-mainnet.alchemyapi.io/v2/0EvZQA7WvDYf40cz476eEIh348_PcZJu' // For Production build
     : 'https://eth-mainnet.alchemyapi.io/v2/p56_qS-3ABDzHGg9kQ-kyqqbQVNJmzUB' // For Local Development
