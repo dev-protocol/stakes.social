c195cd9c30491c59e0bf50fa29af340431df7e80
Cold-A-Muse <daan.998@hotmail.com>
2021-03-04 08:48:52 +0700

    Q/A Content

--- packages/web/src/components/organisms/Incubator/Tips/index.tsx
+++ packages/web/src/components/organisms/Incubator/Tips/index.tsx
@@ -8,27 +8,22 @@ const tips = [
   {
     question: 'What is Stakes Social?',
     answer:
-      'Stakes Social is Dev Protocol’s user facing application where OSS projects and patrons connect. \
-      OSS projects use Stakes Social to tokenize, obtain patrons, build communities, and incentive stakeholders \
-      in order to grow their project. Patrons use Stakes Social to stake the DEV token for OSS projects they support.'
+      'Stakes Social is Dev Protocol’s user facing application where OSS projects and patrons connect. OSS projects use Stakes Social to tokenize, obtain patrons, build communities, and incentive stakeholders in order to grow their project. Patrons use Stakes Social to stake the DEV token for OSS projects they support.'
   },
   {
     question: 'What is Dev Protocol?',
     answer:
-      'Dev is a blockchain protocol that allows Open Source projects to sustainably fund their development. Patrons stake the DEV token for projects they want to fund. The protocol then mints new DEV tokens that are split between the OSS project and Patron. Dev Protocol is the only platform that allows both Patrons and OSS projects to earn money by supporting each other. The Protocol’s APY is based on DEV staked on the protocol and OSS projects onboarded.'
+      'Dev Protocol is a blockchain protocol that allows Open Source projects to sustainably fund their development. Patrons stake the DEV token for projects they want to fund. The protocol then mints new DEV tokens that are split between the OSS project and Patron. Dev Protocol is the only platform that allows both Patrons and OSS projects to earn money by supporting each other. The Protocol’s APY is based on DEV staked on the protocol and OSS projects onboarded.'
   },
   {
     question: 'How much time is remaining in the round?',
     answer:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      'We offer projects the opportunity to claim their project at any time, but we recommend that maintainers claim at the end of the round to receive the maximum funding.'
   },
   {
     question: 'What happens at the end of the round?',
     answer:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  \
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur \
-      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      'At the end of the round projects the Dev Protocol Treasury removes its stakes in each project. When this occurs your project will stop accumulating funding. In order to receive more funding claim your project and build a community of Patrons to start staking DEV tokens for your project!'
   }
 ]
 
--- packages/web/src/pages/incubator/QA/index.tsx
+++ packages/web/src/pages/incubator/QA/index.tsx
@@ -6,7 +6,7 @@ import IncubatorHeader from 'src/components/organisms/Incubator/Header'
 import Hr from 'src/components/organisms/Incubator/molecules/Hr'
 import Footer from 'src/components/organisms/Incubator/Footer'
 import BackArrow from 'src/components/organisms/Incubator/molecules/BackArrow'
-import { Span } from 'src/components/organisms/Incubator/Typography'
+import { H1Large, H2M, Text1L } from 'src/components/organisms/Incubator/Typography'
 
 const Container = styled.div`
   display: flex;
@@ -27,7 +27,8 @@ const BackArrowContainer = styled.div`
 
 const QAGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 0.5fr 1fr;
+  column-gap: 124px;
   padding-bottom: 4em;
 `
 
@@ -49,50 +50,46 @@ const QAPage = () => {
       </Container>
 
       <Container>
-        <Span fontSize="40px" fontWeight="bold">
-          {'Q&As'}
-        </Span>
+        <H1Large>{'Q&As'}</H1Large>
 
         <QAGrid style={{ paddingTop: '2em' }}>
-          <Span fontSize="24px">What is Dev Protocol?</Span>
-          <Span fontSize="20px">
-            Dev is a blockchain protocol that allows Open Source projects to sustainably fund their development. Patrons
-            stake the DEV token for projects they want to fund. The protocol then mints new DEV tokens that are split
-            between the OSS project and Patron. Dev Protocol is the only platform that allows both Patrons and OSS
-            projects to earn money by supporting each other. The Protocol’s APY is based on DEV staked on the protocol
-            and OSS projects onboarded.
-          </Span>
-        </QAGrid>
-        <Hr />
-
-        <QAGrid style={{ paddingTop: '1em' }}>
-          <Span fontSize="24px">What is Stakes Social?</Span>
-          <Span fontSize="20px">
+          <H2M>What is Stakes Social?</H2M>
+          <Text1L>
             Stakes Social is Dev Protocol’s user facing application where OSS projects and patrons connect. OSS projects
             use Stakes Social to tokenize, obtain patrons, build communities, and incentive stakeholders in order to
             grow their project. Patrons use Stakes Social to stake the DEV token for OSS projects they support.
-          </Span>
+          </Text1L>
         </QAGrid>
         <Hr />
 
         <QAGrid style={{ paddingTop: '1em' }}>
-          <Span fontSize="24px">How much time is remaining in the round?</Span>
-          <Span fontSize="20px">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.
-          </Span>
+          <H2M>What is Dev Protocol?</H2M>
+          <Text1L>
+            Dev Protocol is a blockchain protocol that allows Open Source projects to sustainably fund their
+            development. Patrons stake the DEV token for projects they want to fund. The protocol then mints new DEV
+            tokens that are split between the OSS project and Patron. Dev Protocol is the only platform that allows both
+            Patrons and OSS projects to earn money by supporting each other. The Protocol’s APY is based on DEV staked
+            on the protocol and OSS projects onboarded.
+          </Text1L>
         </QAGrid>
         <Hr />
 
         <QAGrid style={{ paddingTop: '1em' }}>
-          <Span fontSize="24px">What happens at the end of the round?</Span>
-          <Span fontSize="20px">
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.
-          </Span>
+          <H2M>How much time is remaining in the round?</H2M>
+          <Text1L>
+            We offer projects the opportunity to claim their project at any time, but we recommend that maintainers
+            claim at the end of the round to receive the maximum funding.
+          </Text1L>
+        </QAGrid>
+        <Hr />
+
+        <QAGrid style={{ paddingTop: '1em' }}>
+          <H2M>What happens at the end of the round?</H2M>
+          <Text1L>
+            At the end of the round projects the Dev Protocol Treasury removes its stakes in each project. When this
+            occurs your project will stop accumulating funding. In order to receive more funding claim your project and
+            build a community of Patrons to start staking DEV tokens for your project!
+          </Text1L>
         </QAGrid>
       </Container>
 
