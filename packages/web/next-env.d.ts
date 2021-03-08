395594d91f9ec90170ed828899e5e1797097ae02
kawakami <saruyama.monki@gmail.com>
2021-03-08 14:00:05 +0900

    modified code from code review

--- packages/web/next-env.d.ts
+++ packages/web/next-env.d.ts
@@ -1,14 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-interface Window {
-  gtag(type: 'config', googleAnalyticsId: string, { page_path: string })
-  gtag(
-    type: 'event',
-    eventAction: string,
-    fieldObject: {
-      event_label: string
-      event_category: string
-      value?: string
-    }
-  )
-}
