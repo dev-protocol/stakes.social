cf644ff1703dad3c935b0d95b997cad646948d56
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2021-03-09 09:27:38 +0900

    Deploy 2021.03.09 (#1087)
    * fix(deps): update dependency date-fns to v2.19.0 (#1066)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * fix(deps): update dependency react-confetti to v6.0.1 (#1065)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * fix(deps): update dependency next to v10.0.8 (#1068)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * fix(deps): update dependency antd to v4.13.1 (#1069)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * chore(deps): update dependency @types/react to v16.14.5 (#1070)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * chore(deps): update graphqlcodegenerator monorepo (#1071)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Google analytics (#1042)
    
    * Add to google analytics
    
    * Revert "Add to google analytics"
    
    This reverts commit 88d1068d
    
    * Add to google analytics
    
    * Add to google analytics
    
    * Add to GDPR
    
    * modified GA code from code review.
    
    * modified GA code
    
    * modified code from code review
    
    * chore(deps): update dependency @types/react to v17 (#1074)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * chore(deps): update dependency @types/react-dom to v17 (#1075)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * chore(deps): update dependency concurrently to v6 (#1076)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * fix(deps): update dependency ethers to v5.0.32 (#1079)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * chore(deps): update typescript-eslint monorepo to v4.17.0 (#1081)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * chore(deps): update dependency @types/react-dom to v17.0.2 (#1083)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { GA_ID } from '../lib/gtag'
 
 export default class extends Document {
   static getInitialProps = async (ctx: DocumentContext) => {
@@ -48,6 +49,50 @@ export default class extends Document {
           <meta name="msapplication-TileColor" content="#2F80ED" />
           <meta name="msapplication-TileImage" content="//asset.stakes.social/ms-icon-144x144.png" />
           <meta name="theme-color" content="#2F80ED" />
+          {/* Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`
+            }}
+          />
+          {/* cookieconsent */}
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css"
+            type="text/css"
+          />
+          <script
+            async
+            src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"
+            data-cfasync="false"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.addEventListener("load",async () => {
+                      window.cookieconsent.initialise({
+                          "palette": {
+                              "popup": {
+                                  "background": "#fafafa"
+                              },
+                              "button": {
+                                  "background": "#000000",
+                                  "text": "#ffffff"
+                              }
+                          },
+                          "showLink": false,
+                          "position": "bottom-right"
+                      });
+                  });`
+            }}
+          />
         </Head>
         <body>
           <Main />
---
+++ packages/web/src/types/GoogleAnalyticsEvent.d.ts
@@ -0,0 +1,13 @@
+type ContactEvent = {
+  action: 'submit_form'
+  category: 'Contact'
+  label: string
+}
+
+type ClickEvent = {
+  action: 'click'
+  category: 'Other'
+  label: string
+}
+
+export type Event = ContactEvent | ClickEvent
