782579bf17030ed57411a33f882bda6d39d46be5
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2021-06-14 15:05:19 +0900

    Layout fix

--- packages/web/src/components/organisms/PossessionOutline/index.tsx
+++ packages/web/src/components/organisms/PossessionOutline/index.tsx
@@ -28,79 +28,86 @@ export const PossessionOutline = ({ className, propertyAddress }: Props) => {
 
   return (
     <div className={className}>
-      <Row gutter={[24, 24]} justify={'space-between'}>
-        <Col span={6}>
+      <Row gutter={[24, 24]} justify={'start'}>
+        <Col xs={24} sm={12} md={6} lg={6}>
           <Card>
             <Statistic
               title="Total Staking Amount"
               value={totalStakingAmount ? totalStakingAmount.toNumber() : 'N/A'}
               precision={2}
               suffix={totalStakingAmountCurrency}
+              valueStyle={{ textAlign: 'right' }}
             />
           </Card>
         </Col>
-        <Col span={6}>
+        <Col xs={24} sm={12} md={6} lg={6}>
           <Card>
             <Statistic
               title="Total Rewards"
               value={totalRewardsAmount && totalRewardsAmount.toNumber()}
               precision={2}
               suffix={totalRewardsAmountCurrency}
+              valueStyle={{ textAlign: 'right' }}
             />
           </Card>
         </Col>
 
-        <Col span={6}>
+        <Col xs={24} sm={12} md={6} lg={6}>
           <Card>
             <Statistic
               title="Lifetime Reward"
               value={lifetimeReward ? lifetimeReward.toNumber() || 0 : 'N/A'}
               precision={2}
               suffix="DEV"
+              valueStyle={{ textAlign: 'right' }}
             />
           </Card>
         </Col>
 
-        <Col span={6}>
+        <Col xs={24} sm={12} md={6} lg={6}>
           <Card>
             <Statistic
               title="Receivable Amount"
               value={withdrawableAmount ? withdrawableAmount.toNumber() || 0 : 'N/A'}
               precision={2}
               suffix="DEV"
+              valueStyle={{ textAlign: 'right' }}
             />
           </Card>
         </Col>
 
-        <Col span={8}>
+        <Col xs={24} sm={12} md={6} lg={6}>
           <Card>
             <Statistic
               title="Your Staking Share"
               value={myStakingAmount && totalStakingAmount ? stakingShare || 0 : 'N/A'}
               precision={2}
               suffix="%"
+              valueStyle={{ textAlign: 'right' }}
             />
           </Card>
         </Col>
 
-        <Col span={8}>
+        <Col xs={24} sm={12} md={6} lg={6}>
           <Card>
             <Statistic
               title="Your Staking Amount"
               value={myStakingAmount ? myStakingAmount.toNumber() : 'N/A'}
               precision={2}
               suffix={myStakingAmountCurrency}
+              valueStyle={{ textAlign: 'right' }}
             />
           </Card>
         </Col>
 
-        <Col span={8}>
+        <Col xs={24} sm={12} md={6} lg={6}>
           <Card>
             <Statistic
               title="Your Rewards"
               value={myStakingRewardAmount && myStakingRewardAmount.toNumber()}
               precision={2}
               suffix={myStakingRewardAmountCurrency}
+              valueStyle={{ textAlign: 'right' }}
             />
           </Card>
         </Col>
