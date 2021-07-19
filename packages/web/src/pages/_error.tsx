39938913e3d649c1ab12dad7311d5ca3383da53b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2021-07-19 09:40:12 +0900

    feat: added custom error page

--- packages/web/src/pages/_error.tsx
+++ packages/web/src/pages/_error.tsx
@@ -1,11 +1,8 @@
-function Error({ statusCode }) {
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <div>{statusCode ? `An error ${statusCode} occurred on server` : 'An error occurred on client'}</div>
+import { NextPage } from 'next'
+import React from 'react'
+
+const CustomErrorPage: NextPage = () => {
+  return <div>An error occurred on client</div>
 }
 
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
-}
-
-export default Error
+export default CustomErrorPage
