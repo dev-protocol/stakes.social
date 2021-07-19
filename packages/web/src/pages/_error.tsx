dcf6455e3d174017010f7eed1831d9423a73e093
k3nt0w <kento.watanabe.11@gmail.com>
2021-07-19 21:13:32 +0900

    fix: remove error

--- packages/web/src/pages/_error.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next'
-import React from 'react'
-
-const CustomErrorPage: NextPage = () => {
-  return <div>An error occurred on client</div>
-}
-
-export default CustomErrorPage
