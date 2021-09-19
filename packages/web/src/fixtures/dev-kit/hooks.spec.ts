df973025076b9e83fb7610dfb928dd472a1da834
k3nt0w <kento.watanabe.11@gmail.com>
2021-09-19 15:53:46 +0900

    feat: useDepositToPosition test

--- packages/web/src/fixtures/dev-kit/hooks.spec.ts
+++ packages/web/src/fixtures/dev-kit/hooks.spec.ts
@@ -27,7 +27,8 @@ import {
   useGetSTokenPositions,
   useGetStokenRewards,
   useApprove,
-  useDepositToProperty
+  useDepositToProperty,
+  useDepositToPosition
 } from './hooks'
 import { useCurrency } from 'src/fixtures/currency/functions/useCurrency'
 import useSWR from 'swr'
@@ -1013,6 +1014,35 @@ describe('dev-kit hooks', () => {
       const error = new Error(errorMessage)
       ;(useSWR as jest.Mock).mockImplementation(() => ({ data, error }))
       const { result } = renderHook(() => useDepositToProperty('property-address', '100000'))
+      expect(result.current.error).toBe(error)
+      expect(result.current.error?.message).toBe(errorMessage)
+    })
+  })
+
+  describe('useDepositToPosition', () => {
+    test('data is undefined', () => {
+      const data = undefined
+      const error = undefined
+
+      ;(useSWR as jest.Mock).mockImplementation(() => ({ data, error }))
+      const { result } = renderHook(() => useDepositToPosition('property-address', '100000', '2'))
+      expect(result.current.ok).toBe(data)
+    })
+
+    test('success fetching data', () => {
+      const data = true
+      const error = undefined
+      ;(useSWR as jest.Mock).mockImplementation(() => ({ data, error }))
+      const { result } = renderHook(() => useDepositToPosition('property-address', '100000', '2'))
+      expect(result.current.ok).toBe(data)
+    })
+
+    test('failure fetching data', () => {
+      const data = undefined
+      const errorMessage = 'error'
+      const error = new Error(errorMessage)
+      ;(useSWR as jest.Mock).mockImplementation(() => ({ data, error }))
+      const { result } = renderHook(() => useDepositToPosition('property-address', '100000', '2'))
       expect(result.current.error).toBe(error)
       expect(result.current.error?.message).toBe(errorMessage)
     })
