d86ceb9eb775c685869c2e30390585cfb1f38300
k3nt0w <kento.watanabe.11@gmail.com>
2021-09-21 13:48:35 +0900

    feat: convert to stokens page

---
+++ packages/web/src/pages/convert-to-stokens.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from 'src/components/organisms/Header'
+import { Footer } from '../components/organisms/Footer'
+
+const termsOfUse = () => {
+  return (
+    <>
+      <Header />
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default termsOfUse
