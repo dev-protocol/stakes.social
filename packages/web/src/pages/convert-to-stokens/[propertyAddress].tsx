5dffd63e2cf657bb9c13aa73f356fe8a4fd7e321
k3nt0w <kento.watanabe.11@gmail.com>
2021-09-22 14:01:04 +0900

    fix: use styled compoentn

--- packages/web/src/pages/convert-to-stokens/[propertyAddress].tsx
+++ packages/web/src/pages/convert-to-stokens/[propertyAddress].tsx
@@ -1,17 +1,22 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import styled from 'styled-components'
 import { ConvertToStokens } from 'src/components/organisms/ConvertToStokens'
 import { Header } from 'src/components/organisms/Header'
 import { Footer } from '../../components/organisms/Footer'
+
+const Wrap = styled.div`
+  margin: '12px 0';
+`
 
 const Index = () => {
   const { propertyAddress } = useRouter().query as { propertyAddress: string }
   return (
     <>
       <Header />
-      <div style={{ margin: '12px 0' }}>
+      <Wrap>
         <ConvertToStokens propertyAddress={propertyAddress} />
-      </div>
+      </Wrap>
       <Footer />
     </>
   )
