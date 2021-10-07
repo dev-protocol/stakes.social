6f83e2de29475f58934944b9bf96419ba013e97e
aggre <hiroyuki.aggre@gmail.com>
2021-10-08 02:58:55 +0900

    add "useDetectChain hook"

--- packages/web/src/fixtures/wallet/hooks.spec.ts
+++ packages/web/src/fixtures/wallet/hooks.spec.ts
@@ -1,6 +1,7 @@
 import { renderHook, act } from '@testing-library/react-hooks'
-import { useConnectWallet } from './hooks'
+import { useConnectWallet, useDetectChain } from './hooks'
 import { connectWallet } from 'src/fixtures/wallet/utility'
+import { providers } from 'ethers'
 
 jest.mock('src/fixtures/wallet/utility.ts')
 
@@ -19,5 +20,43 @@ describe('useConnectWallet', () => {
     })
     expect(result.current.isConnected).toBe(false)
     expect(result.current.isConnecting).toBe(false)
+  })
+})
+
+describe('useDetectChain', () => {
+  test('Returns undefined when the detected chainId is not supported', async () => {
+    const mock = { getNetwork: () => Promise.resolve({ chainId: 99999 }) } as unknown as providers.BaseProvider
+    const { result, waitForNextUpdate } = renderHook(() => useDetectChain(mock))
+    await waitForNextUpdate()
+    expect(result.current.chainId).toBe(99999)
+    expect(result.current.name).toBe(undefined)
+  })
+  test('Detect mainnet', async () => {
+    const mock = { getNetwork: () => Promise.resolve({ chainId: 1 }) } as unknown as providers.BaseProvider
+    const { result, waitForNextUpdate } = renderHook(() => useDetectChain(mock))
+    await waitForNextUpdate()
+    expect(result.current.chainId).toBe(1)
+    expect(result.current.name).toBe('main')
+  })
+  test('Detect ropsten', async () => {
+    const mock = { getNetwork: () => Promise.resolve({ chainId: 3 }) } as unknown as providers.BaseProvider
+    const { result, waitForNextUpdate } = renderHook(() => useDetectChain(mock))
+    await waitForNextUpdate()
+    expect(result.current.chainId).toBe(3)
+    expect(result.current.name).toBe('ropsten')
+  })
+  test('Detect arbitrum one main', async () => {
+    const mock = { getNetwork: () => Promise.resolve({ chainId: 42161 }) } as unknown as providers.BaseProvider
+    const { result, waitForNextUpdate } = renderHook(() => useDetectChain(mock))
+    await waitForNextUpdate()
+    expect(result.current.chainId).toBe(42161)
+    expect(result.current.name).toBe('arbitrum-one-main')
+  })
+  test('Detect arbitrum one rinkeby', async () => {
+    const mock = { getNetwork: () => Promise.resolve({ chainId: 421611 }) } as unknown as providers.BaseProvider
+    const { result, waitForNextUpdate } = renderHook(() => useDetectChain(mock))
+    await waitForNextUpdate()
+    expect(result.current.chainId).toBe(421611)
+    expect(result.current.name).toBe('arbitrum-one-rinkeby')
   })
 })
--- packages/web/src/fixtures/wallet/hooks.ts
+++ packages/web/src/fixtures/wallet/hooks.ts
@@ -1,12 +1,13 @@
 import Web3 from 'web3'
 import { cachePath } from './catch-path'
 import { connectWallet, disconnectWallet, getAccountAddress, getBlockNumber } from './utility'
-import { UnwrapFunc } from 'src/fixtures/utility'
+import { UnwrapFunc, whenDefined } from 'src/fixtures/utility'
 import useSWR from 'swr'
 import { useContext, useEffect, useState } from 'react'
 import WalletContext from 'src/context/walletContext'
 import { WEB3_PROVIDER_ENDPOINT } from 'src/fixtures/wallet/constants'
 import { providers } from 'ethers'
+import { UndefinedOr } from '@devprotocol/util-ts'
 
 const nonConnectedWeb3 = new Web3(WEB3_PROVIDER_ENDPOINT)
 const nonConnectedEthersProvider = new providers.JsonRpcProvider(WEB3_PROVIDER_ENDPOINT)
@@ -42,6 +43,25 @@ export const useProvider = () => {
     getAccountAddress(web3).then(x => setAccountAddress(x))
   }, [web3])
   return { web3, ethersProvider, nonConnectedWeb3, nonConnectedEthersProvider, accountAddress }
+}
+
+export const useDetectChain = (ethersProvider?: providers.BaseProvider) => {
+  const [chainId, setChainId] = useState<undefined | number>(undefined)
+  useEffect(() => {
+    whenDefined(ethersProvider, prov => prov.getNetwork().then(r => setChainId(r.chainId)))
+  }, [ethersProvider])
+  const name: UndefinedOr<'main' | 'ropsten' | 'arbitrum-one-main' | 'arbitrum-one-rinkeby'> =
+    chainId === 1
+      ? 'main'
+      : chainId === 3
+      ? 'ropsten'
+      : chainId === 42161
+      ? 'arbitrum-one-main'
+      : chainId === 421611
+      ? 'arbitrum-one-rinkeby'
+      : undefined
+
+  return { chainId, name }
 }
 
 export const useBlockNumberStream = (shouldFetch: boolean) => {
