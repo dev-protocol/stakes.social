42e9ad446e410563892f7e06d02bb9cbf236202f
aggre <hiroyuki.aggre@gmail.com>
2021-10-09 05:24:19 +0900

    implement mechanism that fetching contract address by network name

--- packages/web/src/fixtures/dev-kit/get-contract-address.ts
+++ packages/web/src/fixtures/dev-kit/get-contract-address.ts
@@ -1,13 +1,14 @@
 import { DevkitContract } from '@devprotocol/dev-kit'
 import { RegistryContract } from '@devprotocol/dev-kit'
 import { addresses } from '@devprotocol/dev-kit'
+import { ChainName } from '../wallet/utility'
 
-const cache: Map<'main', Map<string, string>> = new Map()
+const cache: Map<ChainName, Map<string, string>> = new Map()
 
 export const getContractAddress = async (
   client: DevkitContract,
   contract: keyof Omit<RegistryContract, 'contract'>,
-  net: 'main' = 'main'
+  net: ChainName = 'main'
 ): Promise<string> =>
   (async fromCache => {
     if (typeof fromCache === 'string') {
@@ -16,7 +17,17 @@ export const getContractAddress = async (
     if (!cache.get(net)) {
       cache.set(net, new Map())
     }
-    const address = await client.registry(addresses.eth[net]?.registry)[contract]()
+    const map =
+      net === 'main'
+        ? addresses.eth.main
+        : net === 'ropsten'
+        ? addresses.eth.ropsten
+        : net === 'arbitrum-one-main'
+        ? addresses.arbitrumOne.main
+        : net === 'arbitrum-one-rinkeby'
+        ? addresses.arbitrumOne.rinkeby
+        : addresses.eth.main
+    const address = await client.registry(map.registry)[contract]()
     cache.get(net)?.set(contract, address)
     return address
   })(cache.get(net)?.get(contract))
