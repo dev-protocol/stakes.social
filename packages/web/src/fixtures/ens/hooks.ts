9b7df2b903d86cf50c4ed20fe33726a8a5d9e5f8
aggre <hiroyuki.aggre@gmail.com>
2021-10-19 03:28:57 +0900

    fix

--- packages/web/src/fixtures/ens/hooks.ts
+++ packages/web/src/fixtures/ens/hooks.ts
@@ -3,7 +3,7 @@ import { useProvider } from '../wallet/hooks'
 export const useENS = () => {
   const { nonConnectedEthersProvider } = useProvider()
   const getENS = async (address: string) => {
-    return nonConnectedEthersProvider.lookupAddress(address)
+    return nonConnectedEthersProvider?.lookupAddress(address)
   }
 
   return { getENS }
