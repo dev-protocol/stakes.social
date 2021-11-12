a8c23a0ef4172a59a6d77fcbb7e5ca512bc0a1e2
aggre <hiroyuki.aggre@gmail.com>
2021-11-12 10:35:19 +0900

    remove unused pages (#1823)

--- packages/web/src/components/molecules/Navigation/index.tsx
+++ packages/web/src/components/molecules/Navigation/index.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useCallback } from 'react'
-import styled from 'styled-components'
 import { MenuInfo } from 'rc-menu/lib/interface'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Hamburger from 'src/components/atoms/Svgs/tsx/Hamburger'
 import { NavMenu, NavMenuItem } from './../../atoms/Navigation/index'
 import { useEffectAsync } from 'src/fixtures/utility'
-import { ButtonXs } from 'src/components/organisms/Incubator/Typography'
 
 interface NavigationProps {
   isMenuOpen: boolean
@@ -30,11 +28,6 @@ export const Navigations = [
     pathname: '/create'
   },
   {
-    key: 'governance',
-    label: 'Govern',
-    pathname: '/gov'
-  },
-  {
     key: 'dashboard',
     label: 'Dashboard',
     pathname: '/stats'
@@ -48,17 +41,8 @@ export const Navigations = [
     key: 'grants',
     label: 'Grants',
     pathname: 'https://devprotocol.notion.site/Welcome-to-DEV-DAPP-STARTER-GRANTS-5cb95252f18540258111581ea54d8808'
-  },
-  {
-    key: 'incubator',
-    label: 'Incubator',
-    pathname: '/incubator'
   }
 ]
-
-const IncubatorContainer = styled(NavMenuItem)`
-  margin: 0;
-`
 
 const toKey = (_pathname: string) => Navigations.find(({ pathname }) => pathname === _pathname)?.key
 
@@ -87,10 +71,6 @@ export const Navigation = ({ handleMenuOpen }: NavigationProps) => {
     [setCurrent]
   )
 
-  const filteredNavigations = Navigations.filter(
-    navigation => navigation.pathname !== '/profile' && navigation.pathname !== '/incubator'
-  )
-
   return (
     <>
       {isDesktop && (
@@ -101,23 +81,13 @@ export const Navigation = ({ handleMenuOpen }: NavigationProps) => {
           selectedKeys={[current || '']}
           mode="horizontal"
         >
-          {filteredNavigations.map(nav => (
+          {Navigations.map(nav => (
             <NavMenuItem key={nav.key} style={{ margin: '0' }}>
               <Link href={nav.pathname}>
                 <a style={{ display: 'block', width: 'auto', fontSize: '0.8em' }}>{nav.label}</a>
               </Link>
             </NavMenuItem>
           ))}
-
-          <IncubatorContainer key={'/inubator'}>
-            <Link href="/incubator">
-              <div style={{ transform: 'translateY(-2px)' }}>
-                <ButtonXs>incubator</ButtonXs>
-              </div>
-            </Link>
-
-            {/* <a style={{ display: 'block', width: 'auto', fontSize: '0.8em' }}>incubator</a> */}
-          </IncubatorContainer>
         </NavMenu>
       )}
       {!isDesktop && (
--- packages/web/src/components/organisms/Incubator/molecules/IncubatorContainer/index.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const IncubatorContainer = styled.div`
-  width: 100%;
-  height: 660px;
-`
-
-export default IncubatorContainer
