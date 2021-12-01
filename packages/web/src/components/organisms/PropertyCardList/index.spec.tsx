2dbb1805a24bd78178b41848e123b1b575a2a927
Hideo Hattori <hhatto.jp@gmail.com>
2021-12-01 13:44:46 +0900

    fix mock and update snapshots

--- packages/web/src/components/organisms/PropertyCardList/index.spec.tsx
+++ packages/web/src/components/organisms/PropertyCardList/index.spec.tsx
@@ -4,7 +4,11 @@ import { PropertyCardList } from '.'
 import 'src/__mocks__/window/matchMedia.mock'
 
 jest.mock('src/fixtures/dev-kit/hooks')
+jest.mock('src/fixtures/wallet/hooks')
+jest.mock('src/fixtures/devprtcl/hooks')
+jest.mock('src/fixtures/dev-for-apps/hooks')
 jest.mock('@dev/graphql')
+jest.mock('ethers')
 jest.mock('src/fixtures/currency/hooks')
 
 describe(`${PropertyCardList.name}`, () => {
