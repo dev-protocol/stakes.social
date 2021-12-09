80cf63edec6f1028ce370aee0e52eec720fe6d1a
Hideo Hattori <hhatto.jp@gmail.com>
2021-12-09 13:49:04 +0900

    Remove display estimate gas price (#1920)
    * remove feature of display estimate gas price
    
    * remove not used import
    
    * update snapshots

--- packages/web/src/components/_pages/create/[market]/AuthForm.tsx
+++ packages/web/src/components/_pages/create/[market]/AuthForm.tsx
@@ -1,13 +1,9 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { Form } from 'antd'
 import styled from 'styled-components'
 import Input from 'src/components/molecules/Input'
-import { EstimatedGasNotes } from 'src/components/molecules/EstimatedGasNotes'
-import { whenDefinedAll } from 'src/fixtures/utility'
 import { useProvider } from 'src/fixtures/wallet/hooks'
 import { InfoCircleOutlined, AccountBookOutlined, CodeOutlined, FontColorsOutlined } from '@ant-design/icons'
-import { useGetEstimateGas4CreateAndAuthenticate } from 'src/fixtures/dev-kit/hooks'
-import { useGetEthPrice } from 'src/fixtures/uniswap/hooks'
 import { useRouter } from 'next/router'
 
 export interface Props {
@@ -77,30 +73,14 @@ const InfoContainer = styled.div`
   }
 `
 
-const EstimateGas = styled.span`
-  font-size: 1em;
-  margin-right: 5px;
-`
-const EstimateGasUSD = styled.span`
-  font-size: 0.9em;
-  color: #a0a0a0;
-`
-
-export const AuthForm = ({ onHeaderChange, onSubHeaderChange, onFormDataSubmit, market }: Props) => {
+export const AuthForm = ({ onHeaderChange, onSubHeaderChange, onFormDataSubmit }: Props) => {
   const { asset: initialAsset } = useRouter().query
   const { accountAddress } = useProvider()
-  const { estimateGas } = useGetEstimateGas4CreateAndAuthenticate(market)
   const onFinish = async (values: any) => {
     onFormDataSubmit(values)
     onHeaderChange('Tokenization Review')
     onSubHeaderChange('Check the details before continuing.')
   }
-  const { data: ethPrice } = useGetEthPrice()
-  const estimateGasUSD = useMemo(
-    () => whenDefinedAll([estimateGas, ethPrice], ([gas, eth]) => gas.multipliedBy(eth)),
-    [estimateGas, ethPrice]
-  )
-
   return (
     <div style={{ maxWidth: '760px' }}>
       <Container>
@@ -185,28 +165,12 @@ export const AuthForm = ({ onHeaderChange, onSubHeaderChange, onFormDataSubmit, 
           </div>
 
           <Row>
-            <div style={{ display: 'flex', gridColumn: '1/-1', justifyContent: 'flex-end' }}>
+            <div style={{ display: 'flex', gridColumn: '1/-1', justifyContent: 'center' }}>
               <ButtonContainer>
                 <Submit type="submit" disabled={!accountAddress}>
                   Tokenize
                 </Submit>
               </ButtonContainer>
-            </div>
-            <div style={{ display: 'flex', gridColumn: '1/-1', justifyContent: 'flex-end' }}>
-              <EstimatedGasNotes>
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                  <div>
-                    Gas Fee: <EstimateGas>{estimateGas?.toFixed() || '-'}ETH</EstimateGas>
-                    <EstimateGasUSD>${estimateGasUSD?.toFixed(2) || '-'}</EstimateGasUSD>
-                  </div>
-                  <div>
-                    <p>
-                      <InfoCircleOutlined style={{ marginRight: '5px' }} />
-                      predicted cost
-                    </p>
-                  </div>
-                </div>
-              </EstimatedGasNotes>
             </div>
           </Row>
         </Form>
--- packages/web/src/components/organisms/Withdraw/WithdrawWithEstimate.tsx
+++ packages/web/src/components/organisms/Withdraw/WithdrawWithEstimate.tsx
@@ -1,14 +1,8 @@
 import React, { useEffect, useState, useMemo } from 'react'
-import {
-  useGetMyStakingAmount,
-  useGetMyStakingRewardAmount,
-  useGetEstimateGas4WithdrawStakingAmount
-} from 'src/fixtures/dev-kit/hooks'
-import { toBigNumber, whenDefined, whenDefinedAll } from 'src/fixtures/utility'
-import { useGetEthPrice } from 'src/fixtures/uniswap/hooks'
+import { useGetMyStakingAmount, useGetMyStakingRewardAmount } from 'src/fixtures/dev-kit/hooks'
+import { toBigNumber, whenDefined } from 'src/fixtures/utility'
 import { FormContainer } from 'src/components/molecules/WithdrawTransactForm/FormContainer'
 import { Estimated } from 'src/components/molecules/WithdrawTransactForm/Estimated'
-import { EstimatedGasFeeCard } from 'src/components/molecules/EstimatedGasFeeCard'
 import { Withdraw } from '.'
 
 interface Props {
@@ -24,12 +18,6 @@ export const WithdrawWithEstimate = ({ className, title, propertyAddress }: Prop
   const [withdrawableTokens, setWithdrawableTokens] = useState<string>('')
   const { dev: myStakingRewardAmount } = useGetMyStakingRewardAmount(propertyAddress)
   const { myStakingAmount } = useGetMyStakingAmount(propertyAddress)
-  const { estimateGas } = useGetEstimateGas4WithdrawStakingAmount(propertyAddress, withdrawAmount || '0')
-  const { data: ethPrice } = useGetEthPrice()
-  const estimateGasUSD = useMemo(
-    () => whenDefinedAll([estimateGas, ethPrice], ([gas, eth]) => gas.multipliedBy(eth)),
-    [estimateGas, ethPrice]
-  )
 
   const estimatedValue = useMemo(
     () => (
@@ -58,10 +46,6 @@ export const WithdrawWithEstimate = ({ className, title, propertyAddress }: Prop
     <FormContainer className={className}>
       <Withdraw propertyAddress={propertyAddress} title={title} onChange={onChange} />
       <Estimated title="Staked and Reward Amount">{estimatedValue}</Estimated>
-      <EstimatedGasFeeCard
-        estimatedGasFee={estimateGas ? estimateGas.toFixed(6) : '-'}
-        estimatedGasFeeUSD={estimateGasUSD ? estimateGasUSD.toFixed(2) : ''}
-      />
     </FormContainer>
   )
 }
