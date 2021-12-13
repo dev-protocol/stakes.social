01d6352d107bb816d024534ebb6ca9278d2cd7b7
Hideo Hattori <hhatto.jp@gmail.com>
2021-12-13 14:30:52 +0900

    wip: new property hero component

--- packages/web/src/components/organisms/PropertyHeader/index.tsx
+++ packages/web/src/components/organisms/PropertyHeader/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 import BigNumber from 'bignumber.js'
-import { BuyDevButton } from 'src/components/molecules/BuyButton'
-import { useGetAuthorInformation } from 'src/fixtures/devprtcl/hooks'
-import { WithGradient } from 'src/components/atoms/WithGradient'
-import ReactMarkdown from 'react-markdown'
-import { useClipboard } from 'use-clipboard-copy'
-import { CopyOutlined, CheckCircleTwoTone } from '@ant-design/icons'
-import ShareTweet from '../../atoms/ShareButtons'
-import { usePropertyAuthor, usePropertyName } from 'src/fixtures/dev-kit/hooks'
+import { CoverImageOrGradient } from 'src/components/atoms/CoverImageOrGradient'
+import { usePropertyName, usePropertySymbol } from 'src/fixtures/dev-kit/hooks'
+import { useGetProperty } from 'src/fixtures/dev-for-apps/hooks'
 
 const ResponsivePropertyAddressFrame = styled.div`
   margin: 1rem auto;
+  min-height: 300px;
+  display: flex;
+  align-items: center;
 `
 
-const Header = styled.h1`
+const RoundedCoverImageOrGradient = styled(CoverImageOrGradient)`
+  border-radius: 5px;
+`
+
+const TokenName = styled.h1`
   margin: 0;
+  margin-bottom: 1rem;
   word-break: break-all;
   font-size: 1.4rem;
   @media (min-width: 768px) {
@@ -23,107 +26,23 @@ const Header = styled.h1`
   }
 `
 
-const SubHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
-`
-
-const StyledDivider = styled.div`
-  padding: 0;
-  margin-left: 2px;
-  margin-right: 2px;
-
-  @media (max-width: 768px) {
-    display: none;
-  }
-`
-
-const ResponsiveSubheaderSection = styled.div`
+const PropertySymbolContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  @media (min-width: 768px) {
-    flex-direction: row;
-  }
 `
 
-const ApyContainer = styled.div`
-  display: flex;
-  width: 95%;
-  margin-top: 10px;
-  margin-bottom: 0;
-  justify-content: space-between;
-  align-items: center;
-  @media (min-width: 768px) {
-    width: 80%;
-    justify-content: flex-start;
-  }
+const SymbolName = styled.div`
+  font-size: 2rem;
 `
 
-const ButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  @media (max-width: 768px) {
-    width: 100%;
-    a {
-      display: flex;
-      justify-content: center;
-      width: 100%;
-    }
-  }
+const TokenAddress = styled.div`
+  color: grey;
 `
 
 const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  @media (min-width: 768px) {
-    flex-direction: row;
-  }
-`
-
-const FlexRow = styled.div`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  width: auto;
-  align-self: flex-start;
-  margin-left: 10px;
-  margin-top: 2px;
-
-  @media (min-width: 768px) {
-    align-self: auto;
-    margin-top: 8px;
-  }
-`
-
-const IconContainer = styled.div`
-  svg {
-    width: 16px;
-    height: auto;
-  }
-  @media (min-width: 768px) {
-    margin-left: 2px;
-  }
-`
-
-const CopyContainer = styled.div`
-  display: flex;
-
-  @media (min-width: 768px) {
-    justify-content: center;
-    align-items: center;
-    p {
-      height: 20px;
-      margin-left: 10px;
-      margin-bottom: 0;
-    }
-  }
+  display: grid;
+  grid-template-columns: 220px 1fr;
+  column-gap: 50px;
 `
 
 interface Props {
@@ -132,65 +51,21 @@ interface Props {
   creators?: BigNumber
 }
 
-export const CopyBadge = ({ propertyAddress }: { propertyAddress: string }) => {
-  const badge = `[![Stake to support us](https://badge.devprotocol.xyz/${propertyAddress}/descriptive)](https://stakes.social/${propertyAddress})`
-  const { copied, copy } = useClipboard({
-    copiedTimeout: 1000
-  })
-  const handleCopy = () => {
-    copy(badge)
-  }
-
-  return (
-    <CopyContainer>
-      <ReactMarkdown>{badge}</ReactMarkdown>
-      <FlexRow onClick={handleCopy}>
-        <IconContainer>
-          {!copied && <CopyOutlined />}
-          {copied && <CheckCircleTwoTone twoToneColor="#52c41a" />}
-        </IconContainer>
-        <span style={{ fontSize: '0.8em', marginLeft: '2px' }}>{!copied ? 'Copy' : 'Copied badge!'}</span>
-      </FlexRow>
-    </CopyContainer>
-  )
-}
-
-export const PropertyHeader = ({ propertyAddress, apy, creators }: Props) => {
+export const PropertyHeader = ({ propertyAddress }: Props) => {
   const { name: propertyName } = usePropertyName(propertyAddress)
-  const { author } = usePropertyAuthor(propertyAddress)
-  const { data: dataAuthor } = useGetAuthorInformation(author)
+  const { symbol } = usePropertySymbol(propertyAddress)
+  const { data: dataProperty } = useGetProperty(propertyAddress)
 
   return (
     <ResponsivePropertyAddressFrame>
       <HeaderContainer>
-        <Header>{propertyName ? `${propertyName} Pool` : ''}</Header>
-        {propertyAddress && <CopyBadge propertyAddress={propertyAddress} />}
+        <RoundedCoverImageOrGradient src={dataProperty?.cover_image?.url} />
+        <PropertySymbolContainer>
+          <TokenName>{propertyName ? `${propertyName}` : ''}</TokenName>
+          <SymbolName>{`$${symbol}`}</SymbolName>
+          <TokenAddress>{propertyAddress}</TokenAddress>
+        </PropertySymbolContainer>
       </HeaderContainer>
-      <ShareTweet title={propertyName || 'N/A'}></ShareTweet>
-      <SubHeader>
-        <ApyContainer>
-          <ResponsiveSubheaderSection>
-            <WithGradient>{apy?.dp(0).toNumber() || 'N/A'}%</WithGradient>
-            <StyledDivider />
-            <span>APY Stakers</span>
-          </ResponsiveSubheaderSection>
-          <StyledDivider>|</StyledDivider>
-          <ResponsiveSubheaderSection>
-            <WithGradient> {creators?.dp(0).toNumber() || 'N/A'}%</WithGradient>
-            <StyledDivider />
-            <span>APY Creators</span>
-          </ResponsiveSubheaderSection>
-          <StyledDivider>|</StyledDivider>
-          <ResponsiveSubheaderSection>
-            <WithGradient> {dataAuthor?.karma.toLocaleString() || 'N/A'}</WithGradient>
-            <StyledDivider />
-            <span>Karma</span>
-          </ResponsiveSubheaderSection>
-        </ApyContainer>
-        <ButtonContainer>
-          <BuyDevButton />
-        </ButtonContainer>
-      </SubHeader>
     </ResponsivePropertyAddressFrame>
   )
 }
