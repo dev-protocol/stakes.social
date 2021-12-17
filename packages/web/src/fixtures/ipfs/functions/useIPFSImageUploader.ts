65980d05d68377e2142477ae981f6367325761c7
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-12-17 10:36:18 -0500

    uses storeBlob instead of store

--- packages/web/src/fixtures/ipfs/functions/useIPFSImageUploader.ts
+++ packages/web/src/fixtures/ipfs/functions/useIPFSImageUploader.ts
@@ -6,19 +6,15 @@ export const useIPFSImageUploader = () => {
   const key = 'useIPFSImageUploader'
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<Error>()
-  const upload = useCallback(async (name: string, description: string, image: File) => {
+  const upload = useCallback(async (image: File) => {
     setIsLoading(true)
 
     const client = new NFTStorage({ token: 'API_TOKEN' })
 
     try {
-      const metadata = await client.store({
-        name,
-        description,
-        image
-      })
+      const cid = await client.storeBlob(image)
       message.success({ content: 'upload successful!', key })
-      return metadata.url
+      return cid
     } catch (e) {
       const err = e as Error
       setError(err)
