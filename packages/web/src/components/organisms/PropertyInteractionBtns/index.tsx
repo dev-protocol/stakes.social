fefc386d98755ea9a42a33d0cf3cda43cb75b394
aggre <hiroyuki.aggre@gmail.com>
2021-12-17 23:38:10 +0900

    tweak styling and behavior

--- packages/web/src/components/organisms/PropertyInteractionBtns/index.tsx
+++ packages/web/src/components/organisms/PropertyInteractionBtns/index.tsx
@@ -1,9 +1,7 @@
-import BigNumber from 'bignumber.js'
+import { Button } from 'antd'
 import React, { useState } from 'react'
 import { ResponsiveModal } from 'src/components/atoms/ResponsiveModal'
 import { TransactModalContents } from 'src/components/molecules/TransactModalContents'
-import { useGetMyStakingAmount, usePropertyAuthor } from 'src/fixtures/dev-kit/hooks'
-import { useProvider } from 'src/fixtures/wallet/hooks'
 import styled from 'styled-components'
 
 interface Props {
@@ -11,48 +9,10 @@ interface Props {
 }
 
 const ButtonContainer = styled.div`
-  display: flex;
+  display: grid;
+  gap: 1rem;
+  grid-auto-flow: column;
   align-items: center;
-  @media (max-width: 768px) {
-    width: 100%;
-    a {
-      display: flex;
-      justify-content: center;
-      width: 100%;
-    }
-  }
-`
-
-const StakeButton = styled.button<{ isPropertyStaked?: Boolean }>`
-  padding: 6px 24px;
-  border: none;
-  background-color: rgb(230, 110, 76);
-  color: white;
-  margin-right: 12px;
-  border-radius: 4px;
-  font-weight: 600;
-  width: 100px;
-
-  cursor: ${props => (props.disabled ? 'auto' : 'pointer')};
-  :hover {
-    transition: ease-in-out 0.2s;
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-  }
-  ${props => props.disabled && 'opacity: 0.3'}
-`
-
-const WithdrawButton = styled.button<{ isPropertyStaked?: Boolean }>`
-  justify-content: center;
-  padding: 6px 24px;
-  border: transparent;
-
-  background-color: rgb(230, 110, 76);
-  color: white;
-  cursor: pointer;
-  border-radius: 4px;
-  font-weight: 600;
-  width: 100px;
-  ${props => props.disabled && 'opacity: 0.3'}
 `
 
 interface ModalStates {
@@ -62,10 +22,6 @@ interface ModalStates {
 }
 
 export const PropertyInteractionBtns = ({ propertyAddress }: Props) => {
-  const { author: authorAddress } = usePropertyAuthor(propertyAddress)
-  const { accountAddress } = useProvider()
-  const { myStakingAmount } = useGetMyStakingAmount(propertyAddress)
-
   const [modalStates, setModalStates] = useState<ModalStates>({ visible: false })
   const showModal = (type: 'stake' | 'withdraw' | 'holders') => {
     const contents = <TransactModalContents propertyAddress={propertyAddress} type={type} />
@@ -75,25 +31,16 @@ export const PropertyInteractionBtns = ({ propertyAddress }: Props) => {
   const closeModal = () => {
     setModalStates({ ...modalStates, visible: false })
   }
-  const zeroBigNumber = new BigNumber(0)
 
   return (
     <div>
       <ButtonContainer>
-        <StakeButton
-          disabled={accountAddress === authorAddress}
-          onClick={() => showModal('stake')}
-          isPropertyStaked={typeof myStakingAmount !== 'undefined' && myStakingAmount > zeroBigNumber}
-        >
+        <Button type="primary" onClick={() => showModal('stake')}>
           Stake
-        </StakeButton>
-        <WithdrawButton
-          disabled={accountAddress === authorAddress}
-          onClick={() => showModal('withdraw')}
-          isPropertyStaked={typeof myStakingAmount !== 'undefined' && myStakingAmount > zeroBigNumber}
-        >
+        </Button>
+        <Button type="primary" onClick={() => showModal('withdraw')}>
           Claim
-        </WithdrawButton>
+        </Button>
       </ButtonContainer>
       <ResponsiveModal visible={modalStates.visible} title={modalStates.title} onCancel={closeModal} footer={null}>
         {modalStates.contents}
