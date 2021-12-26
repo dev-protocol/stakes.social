c0f9c48c2ce2fc7a92ad714b61fe62875f6615f0
Hideo Hattori <hhatto.jp@gmail.com>
2021-12-26 16:16:24 +0900

    integrate set stoken uri

--- packages/web/src/pages/positions/[stokenId]/index.tsx
+++ packages/web/src/pages/positions/[stokenId]/index.tsx
@@ -9,16 +9,19 @@ import { Container } from 'src/components/atoms/Container'
 import { Avatar } from 'src/components/molecules/Avatar'
 import { Header } from 'src/components/organisms/Header'
 import { Footer } from 'src/components/organisms/Footer'
+import { ButtonWithGradient } from 'src/components/atoms/ButtonWithGradient'
 import {
   usePropertyAuthor,
   useGetSTokenTokenURI,
   useGetStokenRewards,
   useGetSTokenOwnerOf,
   useGetStokenHeldAt,
-  useGetSTokenPositions
+  useGetSTokenPositions,
+  useSetSTokenTokenURIImage
 } from 'src/fixtures/dev-kit/hooks'
 import { useProvider } from 'src/fixtures/wallet/hooks'
 import { useGetAccount } from 'src/fixtures/dev-for-apps/hooks'
+import { useIPFSImageUploader } from 'src/fixtures/ipfs/hooks'
 
 const { Dragger } = Upload
 
@@ -27,6 +30,7 @@ type Props = {}
 const Wrap = styled.div`
   margin: 1rem auto;
   max-width: 1048px;
+  text-align: center;
 `
 
 const tableColumns = [
@@ -138,6 +142,7 @@ const STokenPosition = ({ sTokenId }: { sTokenId: number }) => {
 }
 
 const STokenPositionDetail = (_: Props) => {
+  const [fileObj, setFileObj] = useState<File | undefined>(undefined)
   const { stokenId: sTokenIdString } = useRouter().query as { stokenId: string }
   const sTokenId = parseInt(sTokenIdString)
   const { positions } = useGetSTokenPositions(sTokenId)
@@ -156,13 +161,15 @@ const STokenPositionDetail = (_: Props) => {
   const draggerProps = {
     name: 'file',
     multiple: false,
+    maxCount: 1,
     onChange(info: any) {
+      console.log(info)
       const { status } = info.file
       if (status !== 'uploading') {
         console.log(info.file, info.fileList)
       }
       if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`)
+        setFileObj(info.file)
       } else if (status === 'error') {
         message.error(`${info.file.name} file upload failed.`)
       }
@@ -170,6 +177,26 @@ const STokenPositionDetail = (_: Props) => {
     onDrop(e: any) {
       console.log('Dropped files', e.dataTransfer.files)
     }
+  }
+
+  const { upload: uploader, error: uploaderError } = useIPFSImageUploader()
+  const { callback: setStokenURIImage } = useSetSTokenTokenURIImage(sTokenId)
+
+  const onSubmit = async () => {
+    // upload image to IPFS
+    if (!fileObj) {
+      message.error(`file upload failed.`)
+      return
+    }
+    const cid = fileObj && uploader(fileObj)
+    if (uploaderError) {
+      return
+    }
+
+    // set token uri
+    setStokenURIImage(await cid)
+      .then(() => message.success('Success to set sToken Image'))
+      .catch(() => message.error('Fail to set sToken Image'))
   }
 
   return (
@@ -195,15 +222,18 @@ const STokenPositionDetail = (_: Props) => {
         </Container>
         <STokenPosition sTokenId={sTokenId} />
         {author === undefined && isAuthor && (
-          <Dragger style={{ width: '100%', height: '100%' }} {...draggerProps}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files
-            </p>
-          </Dragger>
+          <>
+            <Dragger style={{ width: '100%', height: '100%' }} {...draggerProps}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files
+              </p>
+            </Dragger>
+            <ButtonWithGradient onSubmit={onSubmit}>Save</ButtonWithGradient>
+          </>
         )}
       </Wrap>
 
