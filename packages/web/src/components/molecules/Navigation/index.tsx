b9cca9970df595e334ee5ac3b7f97296fbce991f
aggre <hiroyuki.aggre@gmail.com>
2022-01-09 09:38:04 +0900

    Improved nav (#2015)
    * improved nav
    
    * styling update
    
    * fix the broken tests
    
    * tweaks
    
    * style
    
    Co-authored-by: Hideo Hattori <hhatto.jp@gmail.com>

--- packages/web/src/components/molecules/Navigation/index.tsx
+++ packages/web/src/components/molecules/Navigation/index.tsx
@@ -1,15 +1,122 @@
-import React, { useState, useCallback } from 'react'
-import { MenuInfo } from 'rc-menu/lib/interface'
-import { useRouter } from 'next/router'
-import Hamburger from 'src/components/atoms/Svgs/tsx/Hamburger'
-import { NavMenu, NavMenuItem } from './../../atoms/Navigation/index'
-import { useEffectAsync } from 'src/fixtures/utility'
+import React, { useState } from 'react'
+import { useRouter, NextRouter } from 'next/router'
 import { LinkWithNetwork } from 'src/components/atoms/LinkWithNetwork'
+import styled from 'styled-components'
+import { useConnectWallet, useDetectChain, useProvider } from 'src/fixtures/wallet/hooks'
+import { Button, Drawer, Popover } from 'antd'
+import StakesSocial from 'src/components/atoms/Svgs/svg/Stakes-social.svg'
+import { DisconnectOutlined, LinkOutlined, MoreOutlined } from '@ant-design/icons'
+import { Container } from 'src/components/atoms/Container'
+import { ChainName } from 'src/fixtures/wallet/utility'
+import { switchChain } from 'src/fixtures/wallet/switch'
+import { providers } from 'ethers'
+import truncateEthAddress from 'truncate-eth-address'
+import EthereumEthLogo from 'src/components/atoms/Svgs/svg/EthereumEthLogo.svg'
+import ArbitrumLogo from 'src/components/atoms/Svgs/svg/ArbitrumLogo.svg'
 
-interface NavigationProps {
-  isMenuOpen: boolean
-  handleMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
-}
+const Nav = styled.nav``
+
+const NavContainer = styled(Container)`
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.7rem;
+`
+
+const NavIcon = styled(MoreOutlined)`
+  padding: 1rem;
+  background: #2172f3;
+  border-radius: 99px;
+  color: white;
+`
+
+const Logo = styled(StakesSocial)`
+  fill: black;
+`
+
+const LogoText = styled.span`
+  margin-left: 0.5em;
+  font-size: 1.2rem;
+  font-weight: bold;
+`
+
+const Grid = styled.div`
+  display: grid;
+`
+
+const NavOpenedWallet = styled(Grid)`
+  gap: 1rem;
+  border: 4px solid whitesmoke;
+  border-radius: 20px;
+  padding: 1rem;
+`
+
+const NavOpenedC = styled(Grid)`
+  grid-auto-flow: column;
+  justify-content: space-between;
+`
+
+const NavOpenedN = styled(Grid)`
+  grid-auto-flow: row;
+  justify-items: start;
+  gap: 1rem;
+`
+
+const Testnet = styled.h4`
+  margin: 0;
+  opacity: 0.5;
+`
+
+const NetworkSwitch = styled(Button)`
+  padding: 0;
+  display: grid;
+  gap: 0.2rem;
+  grid-auto-flow: column;
+  align-items: center;
+`
+
+const NetworkSwitchWithLogo = styled(NetworkSwitch)`
+  padding: 0;
+  display: grid;
+  grid-template-columns: 2rem auto 1fr;
+  justify-items: center;
+`
+
+const GrayCircle = styled.span`
+  color: lightgray;
+  &::before {
+    content: '●';
+  }
+`
+
+const GreenCircle = styled.span`
+  color: #00b050;
+  &::before {
+    content: '●';
+  }
+`
+
+const NavUl = styled.ul`
+  display: grid;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  gap: 1rem;
+  margin-top: 1rem;
+`
+const NavLi = styled.li`
+  a {
+    display: block;
+    border: 4px solid whitesmoke;
+    border-radius: 20px;
+    padding: 1rem;
+  }
+`
+const LogoWrapper = styled(Grid)`
+  align-items: center;
+  grid-auto-flow: column;
+`
 
 export const Navigations = [
   {
@@ -50,58 +157,102 @@ export const Navigations = [
   }
 ]
 
-const toKey = (_pathname: string) => Navigations.find(({ pathname }) => pathname === _pathname)?.key
+const StakesSocialLogo = () => (
+  <LogoWrapper>
+    <Logo id="headerlogo" height="1.2rem" />
+    <LogoText>Stakes.social</LogoText>
+  </LogoWrapper>
+)
 
-export const Navigation = ({ handleMenuOpen }: NavigationProps) => {
-  const router = useRouter()
-  const [current, setCurrent] = useState(toKey(router?.asPath || Navigations[0].key)) // || Navigations[0].key
-  const [isDesktop, setDesktop] = useState(typeof window !== 'undefined' && window?.innerWidth > 1070)
+const ConnectedOrDisconnected = ({ chainName }: { chainName: ChainName }) => {
+  const { ethersProvider } = useProvider()
+  const { name } = useDetectChain(ethersProvider)
+  return chainName === name ? <GreenCircle /> : <GrayCircle />
+}
 
-  const updateMedia = () => {
-    setDesktop(window.innerWidth > 1070)
+const createSwitchNetwork =
+  (router: NextRouter, provider?: providers.BaseProvider) => (chainName: ChainName) => async () => {
+    const res = await switchChain(chainName, provider)
+    if (res === true) {
+      router.push(`/${chainName}`)
+    }
   }
 
-  useEffectAsync(async () => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('resize', updateMedia)
-      return () => window.removeEventListener('resize', updateMedia)
-    }
-    return setDesktop(true)
-  }, [])
-
-  const handleClick = useCallback(
-    (e: MenuInfo) => {
-      const { key } = e
-      setCurrent(String(key))
-    },
-    [setCurrent]
-  )
+export const Navigation = () => {
+  const router = useRouter()
+  const [open, setOpen] = useState(false)
+  const { isConnected, connect, isConnecting } = useConnectWallet()
+  const { accountAddress, ethersProvider } = useProvider()
+  const switchNetwork = createSwitchNetwork(router, ethersProvider)
 
   return (
-    <>
-      {isDesktop && (
-        <NavMenu
-          style={{ background: 'black', paddingRight: '5rem' }}
-          theme="dark"
-          onClick={handleClick}
-          selectedKeys={[current || '']}
-          mode="horizontal"
-        >
+    <Nav>
+      <NavContainer>
+        <StakesSocialLogo />
+        <NavIcon onClick={() => setOpen(true)} />
+      </NavContainer>
+
+      <Drawer visible={open} zIndex={1} onClose={() => setOpen(false)}>
+        <NavOpenedWallet>
+          <span>Select a network</span>
+          <NavOpenedN>
+            <NetworkSwitchWithLogo type="link" onClick={switchNetwork('ethereum')}>
+              <EthereumEthLogo height="2rem" />
+              <ConnectedOrDisconnected chainName="ethereum" />
+              Ethereum
+            </NetworkSwitchWithLogo>
+            <NetworkSwitchWithLogo type="link" onClick={switchNetwork('arbitrum-one')}>
+              <ArbitrumLogo height="2rem" />
+              <ConnectedOrDisconnected chainName="arbitrum-one" />
+              Arbitrum
+            </NetworkSwitchWithLogo>
+            <Popover
+              content={
+                <>
+                  <NetworkSwitch type="link" onClick={switchNetwork('ropsten')}>
+                    <ConnectedOrDisconnected chainName="ropsten" />
+                    <span>Ropsten</span>
+                  </NetworkSwitch>
+                  <NetworkSwitch type="link" onClick={switchNetwork('arbitrum-rinkeby')}>
+                    <ConnectedOrDisconnected chainName="arbitrum-rinkeby" />
+                    <span>Arbitrum Rinkeby</span>
+                  </NetworkSwitch>
+                </>
+              }
+              title="Testnet"
+              trigger="hover"
+              placement="topLeft"
+            >
+              <Testnet>Testnet</Testnet>
+            </Popover>
+          </NavOpenedN>
+          <NavOpenedC>
+            <span>
+              {isConnected && accountAddress ? (
+                <>
+                  <LinkOutlined /> Connected to {truncateEthAddress(accountAddress)}
+                </>
+              ) : (
+                <>
+                  <DisconnectOutlined /> Disconnected to a wallet
+                </>
+              )}
+            </span>
+          </NavOpenedC>
+          <Button onClick={connect} loading={isConnecting} type={isConnected ? 'link' : 'primary'}>
+            {isConnected ? 'Connected' : 'Connect'}
+          </Button>
+        </NavOpenedWallet>
+        <NavUl>
           {Navigations.map(nav => (
-            <NavMenuItem key={nav.key} style={{ margin: '0' }}>
-              <LinkWithNetwork href={nav.pathname} rewrite={nav.rewrite}>
-                <a style={{ display: 'block', width: 'auto', fontSize: '0.8em' }}>{nav.label}</a>
+            <NavLi key={nav.key} style={{ margin: '0' }}>
+              <LinkWithNetwork href={nav.pathname} rewrite={nav.rewrite} passHref>
+                <a>{nav.label}</a>
               </LinkWithNetwork>
-            </NavMenuItem>
+            </NavLi>
           ))}
-        </NavMenu>
-      )}
-      {!isDesktop && (
-        <Hamburger
-          style={{ marginRight: '0.5em', cursor: 'pointer' }}
-          onClick={() => handleMenuOpen(prevMenuOpen => !prevMenuOpen)}
-        />
-      )}
-    </>
+        </NavUl>
+      </Drawer>
+    </Nav>
   )
 }
--- packages/web/src/components/organisms/Header/index.tsx
+++ packages/web/src/components/organisms/Header/index.tsx
@@ -1,58 +1,7 @@
 import * as React from 'react'
-import { BrandLogo } from 'src/components/atoms/BrandLogo'
 import styled from 'styled-components'
-import { Navigation, Navigations } from 'src/components/molecules/Navigation'
-import { useState } from 'react'
+import { Navigation } from 'src/components/molecules/Navigation'
 import { EarlyAccess } from 'src/components/atoms/EarlyAccess'
-import { useConnectWallet, useProvider } from 'src/fixtures/wallet/hooks'
-import { AccountBtn, Connecting } from 'src/components/atoms/Navigation'
-import { useRouter } from 'next/router'
-import { LinkWithNetwork } from 'src/components/atoms/LinkWithNetwork'
-
-interface Props {}
-
-const HeaderContainer = styled.header`
-  background-color: black;
-  width: 100%;
-`
-const Top = styled.header`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid black;
-`
-
-const Logo = styled.div`
-  display: flex;
-
-  @media (max-width: 1070px) {
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%, 50%);
-    margin-top: -0.3em;
-    margin-right: 0;
-  }
-`
-
-const HeaderRight = styled.div`
-  display: flex;
-  justify-content: right;
-`
-
-const NavigationItem = styled.div`
-  padding: 5px 0;
-  border-top: 1px solid;
-  border-color: rgba(255, 255, 255, 0.6);
-
-  a {
-    display: block;
-    width: auto;
-    color: white;
-  }
-`
-const NavigationMenu = styled.div`
-  padding: 0 1em;
-`
 
 export const Container = styled.div`
   width: 100%;
@@ -62,68 +11,11 @@ export const Container = styled.div`
   flex-grow: 1;
 `
 
-const toKey = (_pathname: string) => Navigations.find(({ pathname }) => pathname === _pathname)?.key
-
-export const Header = (_: Props = {}) => {
-  const router = useRouter()
-  const [current] = useState(toKey(router?.asPath || Navigations[0].key)) // || Navigations[0].key
-  const [isMenuOpen, setMenuOpen] = useState(false)
-  const { isConnected, connect, isConnecting } = useConnectWallet()
-  const { accountAddress } = useProvider()
-
-  const accountBtnClick = async () => {
-    connect()
-  }
-
+export const Header = () => {
   return (
-    <div style={{ position: 'sticky', top: 0, width: '100%', zIndex: 5 }}>
-      <HeaderContainer>
-        <Container>
-          <Top>
-            <Logo>
-              <BrandLogo />
-            </Logo>
-
-            <HeaderRight>
-              <Navigation isMenuOpen={isMenuOpen} handleMenuOpen={setMenuOpen} />
-
-              {isConnecting ? (
-                <AccountBtn>
-                  <Connecting>{'Connecting...'}</Connecting>
-                </AccountBtn>
-              ) : !isConnected && !accountAddress ? (
-                <AccountBtn onClick={accountBtnClick}>
-                  <span style={{ fontSize: '0.8em' }}>Sign in</span>
-                </AccountBtn>
-              ) : (
-                <LinkWithNetwork href="/profile" rewrite={true}>
-                  <AccountBtn currentRouter={router?.pathname}>
-                    <React.Fragment>
-                      <span style={{ fontSize: '0.8em' }} className="hideOnSmall">
-                        Portfolio
-                      </span>
-                    </React.Fragment>
-                  </AccountBtn>
-                </LinkWithNetwork>
-              )}
-            </HeaderRight>
-          </Top>
-          {isMenuOpen && (
-            <NavigationMenu>
-              {Navigations.map(nav => (
-                <>
-                  <NavigationItem key={nav.key}>
-                    <LinkWithNetwork href={nav.pathname} rewrite={nav.rewrite}>
-                      <span style={current === nav.key ? { color: '#808080' } : { color: '#FFF' }}>{nav.label}</span>
-                    </LinkWithNetwork>
-                  </NavigationItem>
-                </>
-              ))}
-            </NavigationMenu>
-          )}
-        </Container>
-      </HeaderContainer>
+    <header>
+      <Navigation />
       <EarlyAccess />
-    </div>
+    </header>
   )
 }
