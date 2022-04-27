37130f387055b3996b6530b38ac8912818c459ad
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-04-27 16:49:55 +0900

    removes early access label

--- packages/web/src/components/organisms/Header/index.tsx
+++ packages/web/src/components/organisms/Header/index.tsx
@@ -1,21 +1,10 @@
 import * as React from 'react'
-import styled from 'styled-components'
 import { Navigation } from 'src/components/molecules/Navigation'
-import { EarlyAccess } from 'src/components/atoms/EarlyAccess'
-
-export const Container = styled.div`
-  width: 100%;
-  margin: 0 auto;
-  padding: 0 1rem;
-  max-width: 1200px;
-  flex-grow: 1;
-`
 
 export const Header = () => {
   return (
-    <header>
+    <header className="container max-w-screen-xl mx-auto px-2">
       <Navigation />
-      <EarlyAccess />
     </header>
   )
 }
